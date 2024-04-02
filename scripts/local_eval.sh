3e8d7562521f061d0cc82d5fd141ed4dd9692add
Surya <surya.dheeshjith@gmail.com>
2024-04-02 14:38:25 -0400

    Moved old yamls

--- scripts/local_eval.sh
+++ scripts/local_eval.sh
@@ -7,8 +7,15 @@ comp="compute=local"
 # GO BOTTOM TO TOP
 
 # local eval
+
+# ./.python-greene submitit_hydra.py $comp exp=eval_unet name="$(date +%F)-eval_unet_nosched" unet.encoder.n_channels=[90,180,360] unet.decoder.n_channels=[360,180,90] +exp/unet/modules/activations@model.encoder.conv_block.activation=capped_leaky_relu +exp/unet/modules/activations@model.decoder.conv_block.activation=capped_leaky_relu
+
+./.python-greene submitit_hydra.py $comp exp=eval_recunet name="$(date +%F)-eval__convnextinvrecunet_nodil_large" unet.encoder.n_channels=[360,180,90] unet.decoder.n_channels=[90,180,360] +exp/unet/modules/blocks@model.encoder.conv_block=conv_next_block +exp/unet/modules/blocks@model.decoder.conv_block=conv_next_block
+
+
+
 # ./.python-greene submitit_hydra.py $comp exp=eval name="$(date +%F)-eval_test"
 
 # ./.python-greene submitit_hydra.py $comp exp=eval_recunet name="$(date +%F)-eval_recunet_119"
 
-./.python-greene submitit_hydra.py $comp exp=eval_norecunet name="$(date +%F)-eval_norecunet_on_vit_ext"
+# ./.python-greene submitit_hydra.py $comp exp=eval_norecunet name="$(date +%F)-eval_norecunet_on_vit_ext"

ed1db53b193ab9c626e85f7e82b534583cae8051
Surya <surya.dheeshjith@gmail.com>
2024-05-03 12:34:02 -0400

    Enabled circular padding for conv blocks

--- src/models/modules/blocks.py
+++ src/models/modules/blocks.py
@@ -24,7 +24,7 @@ class TransposedConvUpsample(torch.nn.Module):
                 kernel_size=upsampling,
                 stride=upsampling,
                 padding=0,
-            )  # check padding
+            )
         )
 
         if activation is not None:
@@ -61,19 +61,23 @@ class BasicConvBlock(torch.nn.Module):
         n_layers: int = 1,
         latent_channels: int = None,
         activation: torch.nn.Module = CappedGELU(),
+        pad = 'circular'
     ):
         super().__init__()
+        assert kernel_size % 2 !=0, "Cannot use even kernel sizes!"
+        
         if latent_channels is None:
             latent_channels = max(in_channels, out_channels)
         convblock = []
+        self.N_pad = int((kernel_size + (kernel_size-1)*(dilation-1) -1) / 2)
+        self.pad = pad
         for n in range(n_layers):
             convblock.append(
                 torch.nn.Conv2d(
                     in_channels=in_channels if n == 0 else latent_channels,
                     out_channels=out_channels if n == n_layers - 1 else latent_channels,
                     kernel_size=kernel_size,
-                    dilation=dilation,
-                    padding="same",
+                    dilation=dilation
                 )
             )
             if activation is not None:
@@ -81,59 +85,14 @@ class BasicConvBlock(torch.nn.Module):
         self.convblock = torch.nn.Sequential(*convblock)
 
     def forward(self, x):
-        return self.convblock(x)
+        # return self.convblock(x)
+        for l in self.convblock:
+            if isinstance(l,nn.Conv2d):
+                x = torch.nn.functional.pad(x,(self.N_pad,self.N_pad,0,0),mode=self.pad)
+                x = torch.nn.functional.pad(x,(0,0,self.N_pad,self.N_pad),mode="constant")
+            x= l(x)
+        return x
 
-
-class ConvGRUBlock(torch.nn.Module):
-    """
-    Code modified from
-    https://github.com/happyjin/ConvGRU-pytorch/blob/master/convGRU.py
-    """
-
-    def __init__(
-        self,
-        in_channels: int = 3,
-        kernel_size: int = 1,
-        downscale_factor: int = 4,
-    ):
-        super().__init__()
-
-        self.channels = in_channels
-        self.conv_gates = torch.nn.Conv2d(
-            in_channels=in_channels + self.channels,
-            out_channels=2 * self.channels,  # for update_gate,reset_gate respectively
-            kernel_size=kernel_size,
-            padding="same",
-        )
-        self.conv_can = torch.nn.Conv2d(
-            in_channels=in_channels + self.channels,
-            out_channels=self.channels,  # for candidate neural memory
-            kernel_size=kernel_size,
-            padding="same",
-        )
-        self.h = torch.zeros(1, 1, 1, 1)
-
-    def forward(self, inputs: Sequence) -> Sequence:
-        if inputs.shape != self.h.shape:
-            self.h = torch.zeros_like(inputs)
-        combined = torch.cat([inputs, self.h], dim=1)
-        combined_conv = self.conv_gates(combined)
-
-        gamma, beta = torch.split(combined_conv, self.channels, dim=1)
-        reset_gate = torch.sigmoid(gamma)
-        update_gate = torch.sigmoid(beta)
-
-        combined = torch.cat([inputs, reset_gate * self.h], dim=1)
-        cc_cnm = self.conv_can(combined)
-        cnm = torch.tanh(cc_cnm)
-
-        h_next = (1 - update_gate) * self.h + update_gate * cnm
-        self.h = h_next
-
-        return inputs + h_next
-
-    def reset(self):
-        self.h = torch.zeros_like(self.h)
 
 
 class ConvNeXtBlock(torch.nn.Module):
@@ -154,15 +113,18 @@ class ConvNeXtBlock(torch.nn.Module):
         upscale_factor: int = 4,
         n_layers: int = 1,
         activation: torch.nn.Module = CappedGELU(),
+        pad='circular'
     ):
         super().__init__()
-
+        assert kernel_size % 2 !=0, "Cannot use even kernel sizes!"
+        self.N_pad = int((kernel_size + (kernel_size-1)*(dilation-1) -1) / 2)
+        self.pad = pad
         # Instantiate 1x1 conv to increase/decrease channel depth if necessary
         if in_channels == out_channels:
             self.skip_module = lambda x: x  # Identity-function required in forward pass
         else:
             self.skip_module = torch.nn.Conv2d(
-                in_channels=in_channels, out_channels=out_channels, kernel_size=1
+                in_channels=in_channels, out_channels=out_channels, kernel_size=1, padding="same"
             )
         # Convolution block
         convblock = []
@@ -172,7 +134,6 @@ class ConvNeXtBlock(torch.nn.Module):
                 out_channels=int(latent_channels * upscale_factor),
                 kernel_size=kernel_size,
                 dilation=dilation,
-                padding="same",
             )
         )
         # LayerNorm
@@ -185,7 +146,6 @@ class ConvNeXtBlock(torch.nn.Module):
                 out_channels=int(latent_channels * upscale_factor),
                 kernel_size=kernel_size,
                 dilation=dilation,
-                padding="same",
             )
         )
         if activation is not None:
@@ -202,7 +162,14 @@ class ConvNeXtBlock(torch.nn.Module):
         self.convblock = torch.nn.Sequential(*convblock)
 
     def forward(self, x):
-        return self.skip_module(x) + self.convblock(x)
+        # return self.skip_module(x) + self.convblock(x)
+        skip = self.skip_module(x)
+        for l in self.convblock:
+            if isinstance(l, nn.Conv2d) and l.kernel_size[0]!=1:
+                x = torch.nn.functional.pad(x,(self.N_pad,self.N_pad,0,0),mode=self.pad)
+                x = torch.nn.functional.pad(x,(0,0,self.N_pad,self.N_pad),mode="constant")
+            x = l(x)
+        return skip + x
 
 
 class ConvNeXtBlockOrig(torch.nn.Module):
@@ -222,6 +189,7 @@ class ConvNeXtBlockOrig(torch.nn.Module):
         latent_channels=0,  # ignored
         dilation=1,
         n_layers=0,  # ignored
+        pad='circular'
     ):
         super().__init__()
         if in_channels == out_channels:
@@ -230,11 +198,13 @@ class ConvNeXtBlockOrig(torch.nn.Module):
             self.skip_module = torch.nn.Conv2d(
                 in_channels=in_channels, out_channels=out_channels, kernel_size=1
             )
+        
+        self.N_pad = int((7 + (7-1)*(dilation-1) -1) / 2)
+        self.pad = pad
         self.dwconv = nn.Conv2d(
             in_channels,
             in_channels,
             kernel_size=7,
-            padding="same",
             groups=in_channels,
             dilation=dilation,
         )  # depthwise conv
@@ -255,6 +225,8 @@ class ConvNeXtBlockOrig(torch.nn.Module):
 
     def forward(self, x):
         input = x
+        x = torch.nn.functional.pad(x,(self.N_pad,self.N_pad,0,0),mode=self.pad)
+        x = torch.nn.functional.pad(x,(0,0,self.N_pad,self.N_pad),mode="constant")
         x = self.dwconv(x)
         x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
         x = self.norm(x)
@@ -272,6 +244,7 @@ class ConvNeXtBlockOrig(torch.nn.Module):
 class ConvNeXtBlockOrig2(torch.nn.Module):
     """
     Actual implementation of ConvNeXt. No dilations here.
+    More layers and larger channels compared to ConvNeXtBlockOrig
 
     DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
     """
@@ -286,28 +259,32 @@ class ConvNeXtBlockOrig2(torch.nn.Module):
         latent_channels=0,  # ignored
         dilation=1,
         n_layers=0,  # ignored
+        pad='circular'
     ):
         super().__init__()
         if in_channels == out_channels:
             self.skip_module = lambda x: x  # Identity-function required in forward pass
         else:
             self.skip_module = torch.nn.Conv2d(
-                in_channels=in_channels, out_channels=out_channels, kernel_size=1
+                in_channels=in_channels, out_channels=out_channels, kernel_size=1, padding="same"
             )
+        self.pad = pad
         self.first_conv = torch.nn.Conv2d(
             in_channels=in_channels,
             out_channels=in_channels * 2,
             kernel_size=3,
-            padding="same",
+            dilation=dilation,
         )
+        self.N_pad1 = int((3 + (3-1)*(dilation-1) -1) / 2)
+
         self.dwconv = nn.Conv2d(
             in_channels * 2,
             in_channels * 2,
             kernel_size=7,
-            padding="same",
             groups=in_channels * 2,
             dilation=dilation,
         )  # depthwise conv
+        self.N_pad2 = int((7 + (7-1)*(dilation-1) -1) / 2)
         self.norm = LayerNorm(in_channels * 2, eps=1e-6)
         self.pwconv1 = nn.Linear(
             in_channels * 2, upscale_factor * in_channels
@@ -325,7 +302,11 @@ class ConvNeXtBlockOrig2(torch.nn.Module):
 
     def forward(self, x):
         input = x
+        x = torch.nn.functional.pad(x,(self.N_pad1,self.N_pad1,0,0),mode=self.pad)
+        x = torch.nn.functional.pad(x,(0,0,self.N_pad1,self.N_pad1),mode="constant")
         x = self.first_conv(x)
+        x = torch.nn.functional.pad(x,(self.N_pad2,self.N_pad2,0,0),mode=self.pad)
+        x = torch.nn.functional.pad(x,(0,0,self.N_pad2,self.N_pad2),mode="constant")
         x = self.dwconv(x)
         x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
         x = self.norm(x)

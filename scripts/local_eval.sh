565674a66af54f1d06f2207340155f9c86e840da
Surya <surya.dheeshjith@gmail.com>
2024-05-11 23:14:24 -0400

    Fixed transpose

--- scripts/local_eval.sh
+++ scripts/local_eval.sh
@@ -16,7 +16,7 @@ comp="compute=local"
 # ./.python-greene submitit_hydra.py $comp exp=eval_unet_global model_name_replace="Foundation ConvNext UNet" network="ConvNext UNet Train1Eval1" name="$(date +%F)-foundation-eval_convnextunet_g1_g1" train_region=global_1 region=global_1 ckpt_path=''
 
 # 3. Swin Global
-# ./.python-greene submitit_hydra.py $comp exp=eval_swin_global network="Foundation Swin Train1Eval1" name="$(date +%F)-foundation-eval_swin_g1_g1" train_region=global_1 region=global_1 ckpt_path=''
+./.python-greene submitit_hydra.py $comp exp=eval_swin_global network="Foundation Swin Train1Eval1 Epoch100" name="$(date +%F)-foundation-eval-swin100_g1_g1" train_region=global_1 region=global_1 ckpt_path='/scratch/sd5313/M2Lines/emulator/Ocean_Emulator/train/2024-05-11-foundation_train_swin_global_1/swin/saved_nets/swin_epoch_100_steps_4_global_1_Test_in_u_v_T_ext_tau_u_tau_v_t_ref__outu_v_T_N_train_4000_Lateral_Data_025_no_smooth.pt'
 
 
 ###########################################################################################
--- scripts/local_train.sh
+++ scripts/local_train.sh
@@ -15,10 +15,10 @@ comp="compute=local"
 # ./.python-greene submitit_hydra.py $comp testing=true exp=train_adamunet_global name="$(date +%F)-local_train_adamunet_global_1" region=global_1 batch_size=16 scheduler=True rand_seed=10
 
 # 2. ConvNext UNet Global
-# ./.python-greene submitit_hydra.py $comp testing=true exp=train_unet_global name="$(date +%F)-local_train_convnextunet_global_1" region=global_1 batch_size=8 scheduler=True rand_seed=10
+# ./.python-greene submitit_hydra.py $comp testing=true exp=train_unet_global name="$(date +%F)-local_train_convnextunet_global_1" region=global_1 batch_size=8 scheduler=True rand_seed=10 exp/modules/blocks@swin.up_sampling_block=transposed_conv_upsample
 
 # 3. Swin Global
-./.python-greene submitit_hydra.py $comp exp=train_swin_global name="$(date +%F)-local_train_swin_global_1" region=global_1 batch_size=16 scheduler=True rand_seed=10
+./.python-greene submitit_hydra.py $comp testing=true exp=train_swin_global name="$(date +%F)-local_train_swin_global_1" region=global_1 batch_size=16 scheduler=True rand_seed=10 exp/modules/blocks@swin.up_sampling_block=transposed_conv_upsample swin.embed_dim=60
 
 
 ###########################################################################################

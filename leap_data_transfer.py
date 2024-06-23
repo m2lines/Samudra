61d426b3ce40a0076644042f301a6790d33d0a61
CI Auto Formatter <you@example.com>
2024-06-23 13:52:47 +0000

    Apply pre-commit fixes

--- leap_data_transfer.py
+++ leap_data_transfer.py
@@ -14,16 +14,16 @@ with open(
 fs = gcsfs.GCSFileSystem(token=token)
 
 # Prediction
-mapper = fs.get_mapper(
-    "gs://leap-persistent/sd5313/convnextpredepoch50.zarr"
+mapper = fs.get_mapper("gs://leap-persistent/sd5313/convnextpredepoch50.zarr")
+ds = xr.open_zarr(
+    "/scratch/sd5313/M2Lines/emulator/Ocean_Emulator/Preds/ConvNext UNet Train3DSurfaceEval3DEpoch50_Train_global_3D_Test_global_3D_all_N_train_4000_Lateral_Data_025_no_smooth/Pred_lateral_Fast_Data_025_global_3D_all_N_samples_4000_rand_seed_1.zarr"
 )
-ds = xr.open_zarr('/scratch/sd5313/M2Lines/emulator/Ocean_Emulator/Preds/ConvNext UNet Train3DSurfaceEval3DEpoch50_Train_global_3D_Test_global_3D_all_N_train_4000_Lateral_Data_025_no_smooth/Pred_lateral_Fast_Data_025_global_3D_all_N_samples_4000_rand_seed_1.zarr')
 
 # Test data
-mapper = fs.get_mapper(
-    "gs://leap-persistent/sd5313/OM4_Horizontal_Regrid_Old.zarr"
+mapper = fs.get_mapper("gs://leap-persistent/sd5313/OM4_Horizontal_Regrid_Old.zarr")
+ds = xr.open_zarr(
+    "/vast/sd5313/data/m2lines/3D_ocean_data/OM4_Horizontal_Regrid_Old.zarr"
 )
-ds = xr.open_zarr('/vast/sd5313/data/m2lines/3D_ocean_data/OM4_Horizontal_Regrid_Old.zarr')
 
 with ProgressBar():
     ds.to_zarr(mapper)
@@ -52,5 +52,3 @@ with ProgressBar():
 #     ds.to_zarr('/vast/sd5313/data/m2lines/3D_ocean_data/test_CMIP6_GFDL-CM4.piControl.r1i1p1f1.zarr',
 #                                  encoding = {v:{'compressor':None} for v in ds.variables},
 #                                  consolidated=True, mode='w')
-
-

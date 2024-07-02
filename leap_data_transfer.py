8e96ad8b54ed14b043886a5d70390746e30879eb
Surya <surya.dheeshjith@gmail.com>
2024-07-02 10:03:49 -0400

    Added new wandb project and 3d viz notebook

--- leap_data_transfer.py
+++ leap_data_transfer.py
@@ -13,20 +13,31 @@ with open(
     token = json.load(f)
 fs = gcsfs.GCSFileSystem(token=token)
 
-# Prediction
-mapper = fs.get_mapper("gs://leap-persistent/sd5313/convnextpredepoch50.zarr")
-ds = xr.open_zarr(
-    "/scratch/sd5313/M2Lines/emulator/Ocean_Emulator/Preds/ConvNext UNet Train3DSurfaceEval3DEpoch50_Train_global_3D_Test_global_3D_all_N_train_4000_Lateral_Data_025_no_smooth/Pred_lateral_Fast_Data_025_global_3D_all_N_samples_4000_rand_seed_1.zarr"
-)
+### Prediction
+# mapper = fs.get_mapper(
+#     "gs://leap-persistent/sd5313/OM4_train_data_means"
+# )
+# ds = xr.open_zarr('/vast/sd5313/data/m2lines/3D_ocean_data/3D_data_means')
 
-# Test data
-mapper = fs.get_mapper("gs://leap-persistent/sd5313/OM4_Horizontal_Regrid_Old.zarr")
-ds = xr.open_zarr(
-    "/vast/sd5313/data/m2lines/3D_ocean_data/OM4_Horizontal_Regrid_Old.zarr"
+# with ProgressBar():
+#     ds.to_zarr(mapper)
+
+mapper = fs.get_mapper(
+    "gs://leap-persistent/sd5313/OM4_train_data_stds"
 )
+ds = xr.open_zarr('/vast/sd5313/data/m2lines/3D_ocean_data/3D_data_stds')
 
 with ProgressBar():
     ds.to_zarr(mapper)
+
+### Test data
+# mapper = fs.get_mapper(
+#     "gs://leap-persistent/sd5313/OM4_Horizontal_Regrid_Old.zarr"
+# )
+# ds = xr.open_zarr('/vast/sd5313/data/m2lines/3D_ocean_data/OM4_Horizontal_Regrid_Old.zarr')
+
+# with ProgressBar():
+#     ds.to_zarr(mapper)
 
 ################################################################################
 #### Data from Leap

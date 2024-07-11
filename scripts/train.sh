9f7d862e89a868f3ef04950d5d47b247cb645316
Surya <surya.dheeshjith@gmail.com>
2024-07-11 10:57:51 -0700

    Shifted data dir arg to exp configs

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,7 +8,7 @@
 # ./.python-perlmutter submitit_hydra.py compute/greene=1x2 compute/greene/node=rtx8000_3hrs exp=train_unet_global_3D wandb.mode=online name="$(date +%F)-train_convnextunet_global_3D_surface_fromdisk" region=global_3D batch_size=16 scheduler=True rand_seed=9
 
 # All
-./.python-perlmutter submitit_hydra.py compute/greene=1x1 compute/greene/node=a100 wandb.mode=online exp=train_unet_global_3D_all name="$(date +%F)-train_convnextunet_global_3D_all_debug_multigpu1x1" region=global_3D batch_size=4 scheduler=True rand_seed=5 unet.ch_width=[80,100,150,300,400]
+./.python-perlmutter submitit_hydra.py compute/greene=4x1 compute/greene/node=a100 wandb.mode=online exp=train_unet_global_3D_all name="$(date +%F)-train_convnextunet_global_3D_all_debug_multigpu4x1" region=global_3D batch_size=4 scheduler=True rand_seed=5 unet.ch_width=[80,100,150,300,400]
 
 ### Swin
 

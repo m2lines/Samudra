6a4f6d527ddb02f2d51971a8109af6923a202cf1
Shubham Gupta <shubhamgupto@gmail.com>
2024-07-27 21:33:06 -0700

    profiling works

--- submitit_hydra.py
+++ submitit_hydra.py
@@ -7,12 +7,14 @@ import sys
 import traceback
 from pathlib import Path
 from re import S
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List, Sequence, Union, Optional, Iterable
 
 import hydra
 import numpy as np
 import submitit
 import submitit.slurm.slurm as submitit_slurm
+
+from submitit.core import utils
 from hydra._internal.config_loader_impl import ConfigLoaderImpl
 from hydra._internal.utils import create_config_search_path
 from hydra.core.plugins import Plugins
@@ -29,14 +31,6 @@ NAME_MAX = 255
 
 @hydra.main(version_base=None, config_name="conf", config_path=CONFIG_PATH)
 def my_app(cfg: DictConfig) -> None:
-
-    # Check if profiling is enabled
-    profiling_enabled = os.getenv('ENABLE_NSYS_PROFILING', '0') == '1'
-    
-    if profiling_enabled:
-        log.info("Profiling is enabled. Pausing profiling before experiment starts.")
-        os.system("srun dcgmi profile --pause")
-
     try:
         env = submitit.JobEnvironment()
         log.info(env.__repr__())
@@ -49,11 +43,6 @@ def my_app(cfg: DictConfig) -> None:
     log.info(f"Beginning experiment [{cfg.experiment}].")
     main_fn(cfg)
     log.info(f"Completed experiment.")
-
-    if profiling_enabled:
-        log.info("Resuming profiling after experiment ends.")
-        os.system("srun dcgmi profile --resume")
-
 
 class LogJobReturnCallback(Callback):
     def __init__(self) -> None:
@@ -104,6 +93,9 @@ if __name__ == "__main__":
     # By setting this, submitit will use this alternative to produce the SBATCH script.
     @property
     def _submitit_command_str(self) -> str:
+        profiling_enabled = os.getenv('ENABLE_NSYS_PROFILING', '0') == '1'
+        if profiling_enabled:
+            log.info("submitit is enabling nsys profiling")
         return " ".join(
             [
                 "--cpu-bind=verbose",
@@ -265,7 +257,6 @@ if __name__ == "__main__":
                     Singleton.get_state(),
                 )
             )
-
         job_dir = str(OmegaConf.select(self.config, job_params[0][1]))
         self.config.hydra.runtime.config_sources[1].path = os.path.join(
             job_dir, ".snapshot", "configs"
@@ -273,12 +264,160 @@ if __name__ == "__main__":
         self.hydra_context.config_loader = ConfigLoaderImpl(
             create_config_search_path(os.path.join(job_dir, ".snapshot", "configs"))
         )
-
+        
         jobs = executor.map_array(self, *zip(*job_params))
         return [j.results()[0] for j in jobs]
 
     Plugins.instance().class_name_to_class[
         "hydra_plugins.hydra_submitit_launcher.submitit_launcher.SlurmLauncher"
     ].launch = launch
+
+    # [HACK] 5
+    # We hijack the sbatch string creation function to insert profiling changes
+    # If the user sets ENABLE_NSYS_PROFILING=1, Nsight is wrapped before the python call in .python-perlmutter
+    # In order to use Nsight, we need to disable NVIDIA DCGMI profiling code
+    # We wrap the experiment srun command with DCGMI profile commands
+    def _make_sbatch_string(
+        command: str,
+        folder: Union[str, Path],
+        job_name: str = "submitit",
+        partition: Optional[str] = None,
+        time: int = 5,
+        nodes: int = 1,
+        ntasks_per_node: Optional[int] = None,
+        cpus_per_task: Optional[int] = None,
+        cpus_per_gpu: Optional[int] = None,
+        num_gpus: Optional[int] = None,  # legacy
+        gpus_per_node: Optional[int] = None,
+        gpus_per_task: Optional[int] = None,
+        qos: Optional[str] = None,  # quality of service
+        setup: Optional[List[str]] = None,
+        mem: Optional[str] = None,
+        mem_per_gpu: Optional[str] = None,
+        mem_per_cpu: Optional[str] = None,
+        signal_delay_s: int = 90,
+        comment: Optional[str] = None,
+        constraint: Optional[str] = None,
+        exclude: Optional[str] = None,
+        account: Optional[str] = None,
+        gres: Optional[str] = None,
+        mail_type: Optional[str] = None,
+        mail_user: Optional[str] = None,
+        nodelist: Optional[str] = None,
+        dependency: Optional[str] = None,
+        exclusive: Optional[Union[bool, str]] = None,
+        array_parallelism: int = 256,
+        wckey: str = "submitit",
+        stderr_to_stdout: bool = False,
+        map_count: Optional[int] = None,  # used internally
+        additional_parameters: Optional[Dict[str, Any]] = None,
+        srun_args: Optional[Iterable[str]] = None,
+        use_srun: bool = True,
+    ) -> str:
+        """Creates the content of an sbatch file with provided parameters
+
+        Parameters
+        ----------
+        See slurm sbatch documentation for most parameters:
+        https://slurm.schedmd.com/sbatch.html
+
+        Below are the parameters that differ from slurm documentation:
+
+        folder: str/Path
+            folder where print logs and error logs will be written
+        signal_delay_s: int
+            delay between the kill signal and the actual kill of the slurm job.
+        setup: list
+            a list of command to run in sbatch before running srun
+        map_size: int
+            number of simultaneous map/array jobs allowed
+        additional_parameters: dict
+            Forces any parameter to a given value in sbatch. This can be useful
+            to add parameters which are not currently available in submitit.
+            Eg: {"mail-user": "blublu@fb.com", "mail-type": "BEGIN"}
+        srun_args: List[str]
+            Add each argument in the list to the srun call
+
+        Raises
+        ------
+        ValueError
+            In case an erroneous keyword argument is added, a list of all eligible parameters
+            is printed, with their default values
+        """
+        nonslurm = [
+            "nonslurm",
+            "folder",
+            "command",
+            "map_count",
+            "array_parallelism",
+            "additional_parameters",
+            "setup",
+            "signal_delay_s",
+            "stderr_to_stdout",
+            "srun_args",
+            "use_srun",  # if False, un python directly in sbatch instead of through srun
+        ]
+        parameters = {k: v for k, v in locals().items() if v is not None and k not in nonslurm}
+        # rename and reformat parameters
+        parameters["signal"] = f"{submitit_slurm.SlurmJobEnvironment.USR_SIG}@{signal_delay_s}"
+        if num_gpus is not None:
+            warnings.warn(
+                '"num_gpus" is deprecated, please use "gpus_per_node" instead (overwritting with num_gpus)'
+            )
+            parameters["gpus_per_node"] = parameters.pop("num_gpus", 0)
+        if "cpus_per_gpu" in parameters and "gpus_per_task" not in parameters:
+            warnings.warn('"cpus_per_gpu" requires to set "gpus_per_task" to work (and not "gpus_per_node")')
+        # add necessary parameters
+        paths = utils.JobPaths(folder=folder)
+        stdout = str(paths.stdout)
+        stderr = str(paths.stderr)
+        # Job arrays will write files in the form  <ARRAY_ID>_<ARRAY_TASK_ID>_<TASK_ID>
+        if map_count is not None:
+            assert isinstance(map_count, int) and map_count
+            parameters["array"] = f"0-{map_count - 1}%{min(map_count, array_parallelism)}"
+            stdout = stdout.replace("%j", "%A_%a")
+            stderr = stderr.replace("%j", "%A_%a")
+        parameters["output"] = stdout.replace("%t", "0")
+        if not stderr_to_stdout:
+            parameters["error"] = stderr.replace("%t", "0")
+        parameters["open-mode"] = "append"
+        if additional_parameters is not None:
+            parameters.update(additional_parameters)
+        # now create
+        lines = ["#!/bin/bash", "", "# Parameters"]
+        for k in sorted(parameters):
+            lines.append(submitit_slurm._as_sbatch_flag(k, parameters[k]))
+        # environment setup:
+        if setup is not None:
+            lines += ["", "# setup"] + setup
+        # commandline (this will run the function and args specified in the file provided as argument)
+        # We pass --output and --error here, because the SBATCH command doesn't work as expected with a filename pattern
+
+        if use_srun:
+            # using srun has been the only option historically,
+            # but it's not clear anymore if it is necessary, and using it prevents
+            # jobs from scheduling other jobs
+            stderr_flags = [] if stderr_to_stdout else ["--error", stderr]
+            if srun_args is None:
+                srun_args = []
+            srun_cmd = submitit_slurm._shlex_join(["srun", "--unbuffered", "--output", stdout, *stderr_flags, *srun_args])
+            command = " ".join((srun_cmd, command))
+
+        profiling_enabled = os.getenv('ENABLE_NSYS_PROFILING', '0') == '1'
+        if profiling_enabled:
+            command = "srun dcgmi profile --pause\n" +\
+                command +\
+                "\nsrun dcgmi profile --resume"
+        lines += [
+            "",
+            "# command",
+            "export SUBMITIT_EXECUTOR=slurm",
+            # The input "command" is supposed to be a valid shell command
+            command,
+            "",
+        ]
+        return "\n".join(lines)
+
+    submitit_slurm._make_sbatch_string=_make_sbatch_string
 
     my_app()

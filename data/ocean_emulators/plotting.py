07060f9097a0373bdd6ebbb3d7b4da016eb38da2
Julius Busecke <julius@ldeo.columbia.edu>
2024-08-12 16:49:26 +0000

    finished filtering

--- data/ocean_emulators/plotting.py
+++ data/ocean_emulators/plotting.py
@@ -1,4 +1,6 @@
+import matplotlib
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 from xarrayutils.plotting import linear_piecewise_scale
 import xarray as xr
 
@@ -58,3 +60,58 @@ def qc_plots(ds: xr.Dataset):
         ax.set_title("Stdv in time of zonal unweighted mean")
 
     plt.show()
+
+
+####### QC plotting for preprocessing #########
+def rotated_vectors_qc_plots(u, v, u_rotated, v_rotated):
+    roi = dict(lev=0, time=100,  y=slice(900, None))
+    kwargs = dict(
+        x='lon', 
+        y='lat',
+        robust=True, 
+        transform=ccrs.PlateCarree()
+    )
+    
+    fig, axarr = plt.subplots(ncols=2, nrows=2, subplot_kw=dict(projection=ccrs.NorthPolarStereo(), facecolor="gray"),)
+    u.isel(**roi).plot(ax=axarr.flat[0], **kwargs)
+    v.isel(**roi).plot(ax=axarr.flat[1], **kwargs)
+    
+    u_rotated.isel(**roi).plot(ax=axarr.flat[2], **kwargs)
+    v_rotated.isel(**roi).plot(ax=axarr.flat[3], **kwargs)
+    
+    for title, ax in zip(['u', 'v', 'u rotated', 'v rotated'], axarr.flat):
+        ax.set_title(title)
+    
+    # compare 'seam line'
+    fig, axarr = plt.subplots(ncols=2, nrows=2, figsize=[8,6])
+    
+
+    def fold_and_compare(ax:matplotlib.axes._axes.Axes, seam_data:xr.DataArray):
+        middle = len(seam_data.x)//2
+        left = seam_data.isel(y=-1, x=slice(0,middle)).data
+        right_flipped = seam_data.isel(y=-1, x=slice(middle, None)).data[::-1]
+        
+        ax.plot(left)
+        ax.plot(right_flipped, ls='--')
+    
+    ax = axarr.flat[0]
+    fold_and_compare(ax, u.isel(**roi).load())
+    ax.set_ylabel('u')
+    ax.set_title('Before rotation: \n should be mirrored')
+    
+    ax = axarr.flat[1]
+    fold_and_compare(ax, v.isel(**roi).load())
+    ax.set_ylabel('v')
+    ax.set_title('Before rotation: \n should be mirrored')
+    
+    ax = axarr.flat[2]
+    fold_and_compare(ax, u_rotated.isel(**roi).load())
+    ax.set_ylabel('u')
+    ax.set_title('After rotation: \n should be aligned')
+    
+    ax = axarr.flat[3]
+    fold_and_compare(ax, v_rotated.isel(**roi).load())
+    ax.set_ylabel('v')
+    ax.set_title('After rotation: \n should be aligned')
+    
+    fig.tight_layout()

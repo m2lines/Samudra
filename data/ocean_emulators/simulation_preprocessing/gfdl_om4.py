c175a1b13485c1176aadd056edd5cbebd7686a2a
James Duncan <jpduncan@berkeley.edu>
2024-08-30 11:43:06 -0700

    Handle data_vars that don't include spatial dims

--- data/ocean_emulators/simulation_preprocessing/gfdl_om4.py
+++ data/ocean_emulators/simulation_preprocessing/gfdl_om4.py
@@ -74,7 +74,7 @@ def om4_preprocessing(zarr_data_path, nc_grid_path, nc_mosaic_path, vertical_dim
         da = ds[var]
         if set(["xh", "yh"]).issubset(da.dims):
             ds_interpolated[var] = da
-        else:
+        elif "xq" in da.dims or "yq" in da.dims:
             # fill the velocities with 0 before interpolation to avoid mismatches in nans
             ds_interpolated[var] = grid.interp_like(da.fillna(0), ds.thetao)
 

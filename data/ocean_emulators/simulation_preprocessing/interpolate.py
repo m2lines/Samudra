29eeb97531fe2cec2be7413064867524738588ec
James Duncan <jpduncan@berkeley.edu>
2024-09-20 09:26:30 -0700

    Don't use xr as a variable name!

--- data/ocean_emulators/simulation_preprocessing/interpolate.py
+++ data/ocean_emulators/simulation_preprocessing/interpolate.py
@@ -15,14 +15,14 @@ def interpolate_to_cell_centers(
         grid: The grid object which will perform the interplation.
 
     """
-    xc, yc = grid.axes["X"].coords["center"], grid.axes["Y"].coords["center"]
-    xr, yr = grid.axes["X"].coords["right"], grid.axes["Y"].coords["right"]
+    xh, yh = grid.axes["X"].coords["center"], grid.axes["Y"].coords["center"]
+    xq, yq = grid.axes["X"].coords["right"], grid.axes["Y"].coords["right"]
     ds_interpolated = xr.Dataset()
     for var in ds.data_vars:
         da = ds[var]
-        if set([xc, yc]).issubset(da.dims):
+        if set([xh, yh]).issubset(da.dims):
             ds_interpolated[var] = da
-        elif xr in da.dims or yr in da.dims:
+        elif xq in da.dims or yq in da.dims:
             # fill the velocities with 0 before interpolation to avoid mismatches in nans
             ds_interpolated[var] = grid.interp_like(da.fillna(0), like)
         if var in ds_interpolated:

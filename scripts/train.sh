ddf84daaf4193700cf7adbc14aaed8a4024debc6
Surya <surya.dheeshjith@gmail.com>
2024-11-22 18:20:15 -0800

    Preliminary expts and swin tests

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,10 +5,10 @@ export BASE_OE_DIR=$PWD
 # 3D
 
 # All history=0 CM4 no fast inp/out 
-# ./.python-perlmutter submitit_hydra.py compute/greene=2x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist0" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=0 epochs=70 exp_num_in=3D_noFast_all exp_num_out=3D_noFast_all --qos=regular
+# ./.python-perlmutter submitit_hydra.py compute/greene=4x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist0" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=0 epochs=70 exp_num_in=3D_noFast_all exp_num_out=3D_noFast_all --qos=regular
 
-# All history=1 CM4 no fast inp/out 
-./.python-perlmutter submitit_hydra.py compute/greene=2x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist1_resumed" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=1 epochs=70 exp_num_in=3D_noFast_all exp_num_out=3D_noFast_all resume_ckpt_path=/pscratch/sd/s/suryad/Ocean_Emulator/train_3D/2024-11-11-convnextunet_CM4_hist1/convnextunet_epoch_38_beststeps_4_global_3D_all_N_train_13800_Lateral_Data_025_no_smooth.pt --qos=regular
+# All history=1 CM4 no fast inp/out - approx 29 hrs for 8 GPUs
+./.python-perlmutter submitit_hydra.py compute/greene=8x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist1_140epochs" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=1 epochs=140 exp_num_in=3D_noFast_all exp_num_out=3D_noFast_all --qos=regular
 
-# All history=1 CM4 All vars
-# ./.python-perlmutter submitit_hydra.py compute/greene=2x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist1_allvars" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=1 epochs=70 --qos=regular
+# All history=1 CM4 All vars - approx 21 hrs for 16 GPUs
+# ./.python-perlmutter submitit_hydra.py compute/greene=8x2 compute/greene/node=a100_30hrs wandb.mode=online exp=train_unet_global_3D_all_CM4 name="$(date +%F)-convnextunet_CM4_hist1_allvars" region=global_3D batch_size=4 scheduler=True rand_seed=15 unet.ch_width=[157,200,250,300,400] hist=1 epochs=70 --qos=regular

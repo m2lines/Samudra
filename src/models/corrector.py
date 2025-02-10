9825e461c9e515827600dbd91c4601acc996bfaf
Surya <surya.dheeshjith@gmail.com>
2025-02-09 21:01:59 -0800

    Salinity unnorm fix

--- src/models/corrector.py
+++ src/models/corrector.py
@@ -2,18 +2,21 @@ from typing import List, Optional
 
 import numpy as np
 import torch
+from einops import rearrange
 
 from constants import TensorMap
+from utils.data import Normalize
 
 
 class Corrector(torch.nn.Module):
-    def __init__(self, corrector_config):
+    def __init__(self, corrector_config, hist):
         super().__init__()
         self.corrector_config = corrector_config
         self.non_negative_corrector_names: Optional[List[str]] = (
             corrector_config.non_negative_corrector_names
         )
         self.tensor_map: TensorMap = TensorMap.get_instance()
+        self.normalize = Normalize.get_instance()
         if self.non_negative_corrector_names is not None:
             self.non_neg_indices = torch.cat(
                 [
@@ -24,10 +27,53 @@ class Corrector(torch.nn.Module):
             )
         else:
             self.non_neg_indices = torch.tensor(np.nan)
+        self.hist = hist
 
     def forward(self, fts):
-        if torch.isnan(self.non_neg_indices).any():
-            indices = self.non_neg_indices.to(fts.device)
-            fts[:, indices, :, :] = torch.relu(fts[:, indices, :, :])
+        if not torch.isnan(self.non_neg_indices).all():
+            # Reshape for unnormalization
+            fts_reshaped = rearrange(
+                fts, "n (hist c) h w -> n hist c h w", hist=self.hist + 1
+            )
+            fts_reshaped = rearrange(fts_reshaped, "n hist c h w -> (n hist) c h w")
 
+            # unnormalize
+            unnormalized_fts = self.normalize.unnormalize_tensor_outputs(fts_reshaped)
+
+            # apply relu
+            indices = self.non_neg_indices.to(unnormalized_fts.device)
+            # unnormalized_fts_cloned = unnormalized_fts.clone()
+            unnormalized_fts[:, indices, :, :] = torch.relu(
+                unnormalized_fts[:, indices, :, :]
+            )
+            # renormalize
+            fts_reshaped_relued = self.normalize.normalize_tensor_outputs(
+                unnormalized_fts
+            )
+            fts_reshaped_relued = rearrange(
+                fts_reshaped_relued,
+                "(n hist) c h w -> n hist c h w",
+                hist=self.hist + 1,
+            )
+            fts_relued = rearrange(
+                fts_reshaped_relued, "n hist c h w -> n (hist c) h w"
+            )
+
+            # assert
+            # fts_norm = self.normalize.normalize_tensor_outputs(
+            # unnormalized_fts_cloned
+            # )
+            # fts_norm = rearrange(
+            #     fts_norm, "(n hist) c h w -> n hist c h w", hist=self.hist + 1
+            # )
+            # fts_norm = rearrange(fts_norm, "n hist c h w -> n (hist c) h w")
+            # print("fts_norm max: ", fts_norm.max())
+            # print("fts max: ", fts.max())
+            # print("fts_norm min: ", fts_norm.min())
+            # print("fts min: ", fts.min())
+            # assert torch.allclose(
+            #     fts_norm, fts, atol=1e-4
+            # )  # TODO: 1e-5 triggers assertion
+
+            fts = fts_relued
         return fts

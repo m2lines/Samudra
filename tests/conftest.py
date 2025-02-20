67b2628a2674d9a5979176754dd60c0a72617157
Alex Merose <alex@openathena.ai>
2025-02-20 12:48:06 -0800

    Created test-bench for data loaders. (#64)
    This PR creates an in-memory dataset and test config to test the shape of training and validation data loaders. These tests abstract away the details of the data loader setup. Thus, the tests should work even if the codebase is refactored.
    
    * Created test-bench for data loaders.
    
    These tests don't work because I don't understand the input data.
    
    rm obstore dep
    
    Self-review.
    
    Added helpful config message.
    
    Progress: passing config + in-memory dataset.
    
    Dummy data loader tests are now passing!
    
    Post-discussion, added data shape tests.
    
    Revert gitignore config.
    
    Revert gitignore config.
    
    Fused config change no longer needed.
    
    Clean up conftest.
    
    Fix misconfiguration.
    
    Cleaner code post-formatter.
    
    YAGI; doing the simple thing now.
    
    * Using the injected config.rootpath rather than computing it myself.
    
    * Better test config, (thanks @suryadheeshjith).
    
    * 2 is the config batch_size!
    
    * Made tests more robust to config changes.
    
    * Intelligible test config (as recommended by Surya)
    
    Co-authored-by: Surya Dheeshjith <41594351+suryadheeshjith@users.noreply.github.com>
    
    * Tests pass after config change.
    
    * Added inference loader tests.
    
    * Fix conftest type issue.
    
    * Updating time frequency.
    
    Co-authored-by: Surya Dheeshjith <41594351+suryadheeshjith@users.noreply.github.com>
    
    * Update data and config times to be spaced every five days.
    
    * Adding typing to datasets test.
    
    * Fixed dual tuple typings.
    
    ---------
    
    Co-authored-by: Surya Dheeshjith <41594351+suryadheeshjith@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,23 @@
+import dataclasses
+import os
+import tempfile
+from typing import Any, Generator, TypedDict
+
+import numpy as np
 import pytest
 import torch
+import xarray as xr
+
+import constants as c
+from config import TrainConfig
+
+
+class DataSource(TypedDict):
+    """In-memory `xarray.Dataset`s needed for tests."""
+
+    data: xr.Dataset
+    means: xr.Dataset
+    stds: xr.Dataset
 
 
 def pytest_addoption(parser):
@@ -25,3 +43,90 @@ def model2_path(request):
 @pytest.fixture(params=["cpu", pytest.param("cuda", marks=pytest.mark.cuda)])
 def device(request):
     return torch.device(request.param)
+
+
+@pytest.fixture(scope="session")
+def data_source() -> DataSource:
+    """Returns in-memory `xarray.Dataset`s for tests."""
+    summer_of_love = xr.cftime_range(
+        "1969-08-10", "1969-12-31", freq="5D", calendar="noleap"
+    )
+
+    coords = {
+        "lon": xr.DataArray(np.arange(0.5, 360, 1), dims=["lon"]),
+        "lat": xr.DataArray(np.arange(-89.24, 90, 1), dims=["lat"]),
+        "time": xr.DataArray(summer_of_love, dims=["time"]),
+    }
+
+    normal = np.random.normal(
+        size=(lat := len(coords["lat"]), lon := len(coords["lon"]))
+    )
+
+    ds: xr.Dataset = xr.Dataset(
+        {
+            # 2D variables
+            var: xr.DataArray(
+                np.random.random([len(summer_of_love), lat, lon]),
+                dims=["time", "lat", "lon"],
+            )
+            for var in ["hfds", "tauuo", "tauvo", "zos"]
+        }
+        | {
+            # 3D variables
+            f"{var}_{lev}": xr.DataArray(
+                np.random.random([len(summer_of_love), lat, lon]),
+                dims=["time", "lat", "lon"],
+            )
+            for var in ["so", "thetao", "uo", "vo"]
+            for lev in c.DEPTH_I_LEVELS
+        }
+        | {
+            # Masks -- make a binary circle mask.
+            f"mask_{lev}": xr.DataArray(
+                np.where(normal > 0.5**lev, 1, 0),
+                dims=["lat", "lon"],
+            )
+            for lev in range(len(c.DEPTH_I_LEVELS))
+        },
+        coords=coords,
+    )
+
+    return {"data": ds, "means": ds.mean("time"), "stds": ds.std("time")}
+
+
+# TODO(alxmrs): Consider yielding multiple test configs.
+@pytest.fixture(scope="session")
+def train_config(
+    data_source: DataSource, pytestconfig: pytest.Config
+) -> Generator[TrainConfig, Any, None]:
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        def _make_path(name_with_ext: str) -> str:
+            return os.path.join(tmpdir, name_with_ext)
+
+        # Write test data to a temporary directory.
+        data_source["data"].to_zarr(_make_path("data.zarr"))
+        data_source["means"].to_netcdf(_make_path("means.netcdf"))
+        data_source["stds"].to_netcdf(_make_path("stds.netcdf"))
+
+        # Open default training script; modify it so it uses the temporary directory.
+        default_config = pytestconfig.rootpath / "configs" / "train_cm4.test.yaml"
+        trainer = TrainConfig.from_yaml(default_config)
+        data_config = dataclasses.replace(
+            trainer.data,
+            data_path=_make_path("data.zarr"),
+            data_means_path=_make_path("means.netcdf"),
+            data_stds_path=_make_path("stds.netcdf"),
+        )
+        experiment_config = dataclasses.replace(
+            trainer.experiment,
+            cluster_data_dir=os.path.join(tmpdir, "cluster_data"),
+        )
+        test_data_trainer = dataclasses.replace(
+            trainer,
+            data=data_config,
+            experiment=experiment_config,
+        )
+
+        # After contextmanager closes, all test data will be automatically cleaned up.
+        yield test_data_trainer

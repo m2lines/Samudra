5f183aed04f1752764279dc53bdb38f2c8cee0c9
Alex Merose <alex@openathena.ai>
2025-03-04 11:12:36 -0800

    Longer deadline for CI.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -134,7 +134,7 @@ def vector_of(max_vec_size: int, min_vec_size=1):
     start_day=datetime.datetime(2000, 5, 1, 12),
     calendar="noleap",
 )
-@settings(deadline=350)
+@settings(deadline=750)
 def test_test_util__data_source_roundtrip(
     data_var_index: int,
     lat: NDArray[np.floating],

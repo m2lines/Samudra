2c6b818106931305ef3aaea58ade95217410e479
Jesse Rusak <jesse@openathena.ai>
2025-03-05 18:21:32 +0000

    Fix off-by-one

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -7,7 +7,7 @@ from train_3D import Trainer
 
 
 @pytest.mark.manual
-def test_trainer__run(train_config: TrainConfig, caplog, benchmark):
+def test_trainer__mini_benchmark(train_config: TrainConfig, caplog, benchmark):
     caplog.set_level(logging.INFO)
     trainer = Trainer(train_config)
 

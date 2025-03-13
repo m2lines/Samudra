140e746f25671def01977b6e927588aaaa4fb40c
Alex Merose <alex@openathena.ai>
2025-03-13 11:15:12 -0700

    Updated zarr tuning script.

--- scripts/open_zarr_tuning.py
+++ scripts/open_zarr_tuning.py
@@ -29,30 +29,48 @@ import zarr
 REMOTE_DATA = "https://nyu1.osn.mghpcc.org/m2lines-pubs/Samudra/OM4"
 
 
-def main(parsed_args: argparse.Namespace):
-    if zcon := parsed_args.zarr_concurrency:
-        zarr.config.set({"async": {"concurrency": zcon, "timeout": None}})
-
+def main(args: argparse.Namespace) -> float:
+    """Calculates elapsed time to open Zarr target over several iterations."""
     chunks = {}
-    if tc := parsed_args.time_chunks:
-        chunks = dict(time=tc)
+    if tc := args.time_chunks:
+        chunks["time"] = tc
 
-    target = parsed_args.target or REMOTE_DATA
+    target = args.target or REMOTE_DATA
 
     start_time = time.perf_counter()
-    for _ in range(8):
-        xr.open_zarr(target, chunks=chunks, consolidated=False)
+    for _ in range(args.n_iters):
+        # Zarr v3 has a runtime config contextmanager.
+        if zc := args.zarr_concurrency:
+            with zarr.config.set({"async.concurrency": zc}):
+                xr.open_zarr(target, chunks=chunks)
+        # Zarr v2 does not.
+        else:
+            xr.open_zarr(target, chunks=chunks)
     end_time = time.perf_counter()
 
-    print(f"ELAPSED: {end_time - start_time:.4f} (8 iter). config: {parsed_args}")
+    return end_time - start_time
+
+
+def Target(candidate: str) -> pathlib.Path | str:
+    """Target can either be a local file or a remote URL."""
+    if "://" in candidate:
+        return candidate
+    return pathlib.Path(candidate)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Experiments to tune OpenZarr")
-    parser.add_argument("--target", type=pathlib.Path, default=None)
-    parser.add_argument("--zarr_concurrency", type=int, default=None)
+    parser.add_argument("--target", type=Target, default=None)
+    parser.add_argument("--n_iters", type=int, default=8)
+    parser.add_argument(
+        "--zarr_concurrency",
+        type=int,
+        default=getattr(zarr, "config", {}).get("async.concurrency"),
+    )
     parser.add_argument("--time_chunks", type=int, default=None)
+    args = parser.parse_args()
 
-    print(f"python-version={sys.version}")
-    print(f"zarr-version={zarr.__version__}")
-    main(parser.parse_args())
+    print(sys.version)
+    print(f"zarr-version={zarr.__version__},xarray-version={xr.__version__}")
+    elapsed = main(args)
+    print(f"ELAPSED: {elapsed:.4f}s. Config: {args}")

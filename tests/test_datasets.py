Masking must be done after normalizing! The low tolerance was converting this mistake.

tests/test_datasets.py
@@ -334,19 +334,18 @@ def test_om4__is_equal_to_v1_data_loader(train_loader_pair: LoaderPair):
     )
     om4_samples = sorted([as_numpy(sample) for sample in om4_loader], key=key)
 
-    atol = 0.01
     for (x_orig, y_orig), (x_new, y_new) in zip(original_samples, om4_samples):
-        x_not_close = np.isclose(x_orig, x_new, atol=atol) == False  # noqa: E712
-        y_not_close = np.isclose(y_orig, y_new, atol=atol) == False  # noqa: E712
+        x_not_close = np.isclose(x_orig, x_new) == False  # noqa: E712
+        y_not_close = np.isclose(y_orig, y_new) == False  # noqa: E712
 
         x_not_close_index = list(zip(*np.where(x_not_close)))
         y_not_close_index = list(zip(*np.where(y_not_close)))
 
-        assert np.allclose(x_orig, x_new, atol=atol), (
+        assert np.allclose(x_orig, x_new), (
             f"{len(x_not_close_index)} values differ: "
             f"{x_orig[x_not_close]} != {x_new[x_not_close]}."
         )
-        assert np.allclose(y_orig, y_new, atol=atol), (
+        assert np.allclose(y_orig, y_new), (
             f"{len(y_not_close_index)} values differ: "
             f"{y_orig[y_not_close]} != {y_new[y_not_close]}."
         )
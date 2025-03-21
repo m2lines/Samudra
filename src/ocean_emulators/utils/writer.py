2b0b4a7a1989420a969fbd0a04176b1ab106f409
Surya <surya.dheeshjith@gmail.com>
2025-03-21 14:19:21 -0700

    Renamed acc_tensor to buffer

--- src/ocean_emulators/utils/writer.py
+++ src/ocean_emulators/utils/writer.py
@@ -20,7 +20,7 @@ class ZarrWriter:
     ):
         self.pred_path = os.path.join(output_dir, "predictions.zarr")
         self.hist = hist
-        self.acc_tensor: torch.Tensor | None = None
+        self.buffer: torch.Tensor | None = None
         self.coords = coords
         self.model_path = model_path
 
@@ -32,22 +32,22 @@ class ZarrWriter:
         pred_tensor = pred_tensor.squeeze(0)
         pred_tensor = rearrange(pred_tensor, "(n c) h w -> n c h w", n=self.hist + 1)
         pred_tensor = self.normalize.unnormalize_tensor_prognostic(pred_tensor)
-        if self.acc_tensor is None:
-            self.acc_tensor = pred_tensor
+        if self.buffer is None:
+            self.buffer = pred_tensor
         else:
-            self.acc_tensor = torch.cat([self.acc_tensor, pred_tensor], dim=0)
+            self.buffer = torch.cat([self.buffer, pred_tensor], dim=0)
 
     def write(self):
         # Write to zarr
-        if self.acc_tensor is None:
+        if self.buffer is None:
             raise ValueError("No tensor to write")
 
         coords: Dict[str, Any] = {k: v for k, v in self.coords.items()}
         # TODO: Replace by actual time so I dont need to fix this downstream
-        coords["time"] = range(self.acc_tensor.shape[0])
+        coords["time"] = range(self.buffer.shape[0])
         ds = xr.Dataset(
             data_vars={
-                var: (["time", "lat", "lon"], self.acc_tensor[:, i, :, :].cpu().numpy())
+                var: (["time", "lat", "lon"], self.buffer[:, i, :, :].cpu().numpy())
                 for i, var in enumerate(self.tensor_map.prognostic_vars_str)
             },
             coords=coords,
@@ -64,4 +64,4 @@ class ZarrWriter:
             )
 
         # Reset
-        self.acc_tensor = None
+        self.buffer = None

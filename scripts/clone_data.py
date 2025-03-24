a1a38ea5f78dc0ac1ffccc3f180add5bf07bd7c4
Alex Merose <alex@openathena.ai>
2025-03-24 16:16:38 -0700

    Compact data during clone.

--- scripts/clone_data.py
+++ scripts/clone_data.py
@@ -16,6 +16,7 @@
 import argparse
 import os
 import pathlib
+from collections import defaultdict
 
 import dask
 import dask.diagnostics
@@ -31,17 +32,36 @@ def robust_open_dataset(target: str, **kwargs) -> xr.Dataset:
 
 
 def compact_dataset(ds: xr.Dataset) -> xr.Dataset:
-    return xr.Dataset()
+    data = ds.copy()
+
+    var_groups = defaultdict(list)
+    for key in data.keys():
+        if "_lev_" in (k := str(key)):
+            base_name = k.split("_lev_")[0]
+            var_groups[base_name].append(k)
+
+    def _parse_level(x) -> float:
+        return float(x.split("_lev_")[1].replace("_", "."))
+
+    for base_var, vars_ in var_groups.items():
+        sorted_vars = sorted(vars_, key=_parse_level)
+        da = xr.concat([data[var] for var in sorted_vars], dim="lev").assign_coords(
+            lev=("lev", data.lev.values)
+        )
+        data[base_var] = da
+        data = data.drop_vars(vars_)
+
+    return data
 
 
 def main(args: argparse.Namespace) -> None:
     """Clones slice of Samudra data at the `dest_root` directory."""
     # Ensure the path/to/dest exists
-    if not args.dest.startswith("gs://"):
+    if not args.dest.startswith("gs://") and not args.dest.startswith("s3://"):
         pathlib.Path(args.dest).mkdir(parents=True, exist_ok=True)
 
     time_slice = slice(args.time_start, args.time_end)
-    output_chunks = dict(time=args.write_time_chunks)
+    output_chunks = dict(time=args.write_time_chunks, lev=19)
 
     for name, dest_fmt in [
         ("OM4", "zarr"),
@@ -55,6 +75,8 @@ def main(args: argparse.Namespace) -> None:
         if name == "OM4":
             data = robust_open_dataset(source, engine="zarr", chunks={"time": 700})
             data = data.isel(time=time_slice)
+            if args.compact_variables:
+                data = compact_dataset(data)
         else:
             data = robust_open_dataset(source, engine="zarr", chunks={})
 

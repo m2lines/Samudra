7ecc5f47b90c17faacac8bb739e36972cbfdd3e1
Jesse Rusak <jesse@openathena.ai>
2025-04-03 11:55:39 -0400

    Fix bug in detection of broken test config, fix test_trainer__mini_2step (#169)
    Our check to see if the "only_configs" were real was (a) not running and
    (b) broken. Fixed the check and also this test which wasn't running and
    was broken in config and also exercised a subtle bug in the new lazy
    loader.

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -15,7 +15,7 @@ def test_trainer__mini_benchmark(trainer_pair: TrainPair, caplog, benchmark):
         trainer.run()
 
 
-@pytest.mark.only_configs("train_cm4_2step.test.yaml")
+@pytest.mark.only_configs("train_default_2step.test.yaml")
 def test_trainer__mini_2step(trainer_pair: TrainPair, caplog):
     caplog.set_level(logging.INFO)
     _, trainer = trainer_pair

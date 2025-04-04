8b6dbdd4abc20f2ac667d2fe0453c45d59aba518
Alex Merose <alex@openathena.ai>
2025-04-04 09:45:04 -0700

    Remove model weight tests
    We haven't used them so far, so I think they are safe to remove.

--- tests/conftest.py
+++ tests/conftest.py
@@ -323,25 +323,6 @@ def check_skip_configs(request, config_name):
                 )
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--model1", action="store", help="Path to the first model .pt file"
-    )
-    parser.addoption(
-        "--model2", action="store", help="Path to the second model .pt file"
-    )
-
-
-@pytest.fixture
-def model1_path(request):
-    return request.config.getoption("--model1")
-
-
-@pytest.fixture
-def model2_path(request):
-    return request.config.getoption("--model2")
-
-
 # Run a test for both CPU and GPU, and allows selecting or skipping CUDA tests.
 @pytest.fixture(
     params=["cpu", pytest.param("cuda", marks=pytest.mark.cuda)], scope="session"

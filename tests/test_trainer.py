d0b120df92128acccab7f3c5e9103ff60343d681
Alex Merose <alex@openathena.ai>
2025-04-07 10:46:32 -0700

    Trainer test should use om4 data

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -20,7 +20,7 @@ def test_trainer__mini_benchmark(trainer_pair: TrainPair, caplog, benchmark):
 
 @pytest.mark.parametrize(
     "data_source,config_name",
-    [("mock", "train_default_2step.test.yaml")],
+    [("remote-om4", "train_default_2step.test.yaml")],
     indirect=True,
 )
 def test_trainer__mini_2step(trainer_pair: TrainPair, caplog):

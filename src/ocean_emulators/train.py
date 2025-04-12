8f0965ae4c55a88512e56b35f9f98d9d063d4c31
Surya <surya.dheeshjith@gmail.com>
2025-04-12 08:15:43 -0700

    Revert "Test gc"
    This reverts commit 47fa73baaf1096e894b1ada7b71f76973387ef16.

--- src/ocean_emulators/train.py
+++ src/ocean_emulators/train.py
@@ -5,7 +5,6 @@
 import argparse
 import contextlib
 import datetime
-import gc
 import logging
 import os
 import time
@@ -444,8 +443,6 @@ class Trainer:
 
         start_time = time.time()
         for epoch in range(self.start_epoch, self.epochs + 1):
-            gc.collect()
-
             # Iterative step training
             if epoch == self.start_epoch or epoch in self.step_transition:
                 cur_step = self.get_current_step(epoch)

Fixed normalization axis and tests

src/ocean_emulators/datasets.py
@@ -176,19 +176,21 @@ class InferenceDataset(Dataset):
         )
         if self.normalize_pre_fill:
             data_in_ds = data_in_src.normalize()
+        else:
+            data_in_ds = data_in_src.get_data()
         data_in_np: np.ndarray = (
             data_in_ds.to_array()
             .transpose("window_dim", "time", "variable", "lat", "lon")
             .to_numpy()
         )
-        data_in_np = rearrange(
-            data_in_np,
-            "window_dim time variable lat lon -> window_dim (time variable) lat lon",
-        )
         data_in: torch.Tensor = torch.from_numpy(data_in_np).float()
         data_in = torch.where(self.wet, data_in, self.nan_fill_value)
         if not self.normalize_pre_fill:
-            data_in = self._prognostic_src.normalize_with(data_in)
+            data_in = self._prognostic_src.normalize_with(data_in, variable_axis=2)
+        data_in = rearrange(
+            data_in,
+            "window_dim time variable lat lon -> window_dim (time variable) lat lon",
+        )
         return data_in
 
     def _get_boundary(self, x_index):
@@ -215,7 +217,9 @@ class InferenceDataset(Dataset):
             self.wet_surface, data_in_boundary, self.nan_fill_value
         )
         if not self.normalize_pre_fill:
-            data_in_boundary = self._boundary_src.normalize_with(data_in_boundary)
+            data_in_boundary = self._boundary_src.normalize_with(
+                data_in_boundary, variable_axis=1
+            )
         return data_in_boundary
 
     def _get_label(self, x_index):
@@ -231,14 +235,14 @@ class InferenceDataset(Dataset):
             .transpose("window_dim", "time", "variable", "lat", "lon")
             .to_numpy()
         )
-        label_np = rearrange(
-            label_np,
-            "window_dim time variable lat lon -> window_dim (time variable) lat lon",
-        )
         label: torch.Tensor = torch.from_numpy(label_np).float()
         label = torch.where(self.wet, label, self.nan_fill_value)
         if not self.normalize_pre_fill:
-            label = self._prognostic_src.normalize_with(label)
+            label = self._prognostic_src.normalize_with(label, variable_axis=2)
+        label = rearrange(
+            label,
+            "window_dim time variable lat lon -> window_dim (time variable) lat lon",
+        )
         return label
 
     def get_coords_dict(self):
@@ -437,15 +441,16 @@ class TrainDataset(Dataset):
             .transpose("window_dim", "time", "variable", "lat", "lon")
             .to_numpy()
         )
+        data_in = torch.from_numpy(data_in).float()
+        data_in = torch.where(self.wet, data_in, self.nan_fill_value)
+        if not self.normalize_pre_fill:
+            data_in = self._prognostic_src.normalize_with(data_in, variable_axis=2)
+
         data_in = rearrange(
             data_in,
             "window_dim time variable lat lon -> \
                 window_dim (time variable) lat lon",
         )
-        data_in = torch.from_numpy(data_in).float()
-        data_in = torch.where(self.wet, data_in, self.nan_fill_value)
-        if not self.normalize_pre_fill:
-            data_in = self._prognostic_src.normalize_with(data_in)
         return data_in
 
     def _get_boundary(self, x_index) -> Boundary:
@@ -473,7 +478,9 @@ class TrainDataset(Dataset):
             self.wet_surface, data_in_boundary, self.nan_fill_value
         )
         if not self.normalize_pre_fill:
-            data_in_boundary = self._boundary_src.normalize_with(data_in_boundary)
+            data_in_boundary = self._boundary_src.normalize_with(
+                data_in_boundary, variable_axis=1
+            )
         return data_in_boundary
 
     def _get_label(self, x_index) -> Prognostic:
@@ -490,15 +497,15 @@ class TrainDataset(Dataset):
             .transpose("window_dim", "time", "variable", "lat", "lon")
             .to_numpy()
         )
+        label = torch.from_numpy(label).float()
+        label = torch.where(self.wet, label, self.nan_fill_value)
+        if not self.normalize_pre_fill:
+            label = label_src.normalize_with(label, variable_axis=2)
         label = rearrange(
             label,
             "window_dim time variable lat lon ->\
                 window_dim (time variable) lat lon",
         ).squeeze()
-        label = torch.from_numpy(label).float()
-        label = torch.where(self.wet, label, self.nan_fill_value)
-        if not self.normalize_pre_fill:
-            label = label_src.normalize_with(label)
         return label
 
 

tests/test_datasets.py
@@ -392,7 +392,12 @@ def test_new_loaders__are_equal_to_v1_data_loader(train_config, loader_version):
 def dataset_input(normalize_pre_fill: bool, nan_fill_value: float):
     # Create data
     coords = {"time": range(10), "lat": range(2), "lon": range(2)}
-    data_array = torch.ones(4, 10, 2, 2)  # [vars, time, lat, lon]
+    times = torch.arange(10)
+    data_array = (
+        torch.repeat_interleave(times, torch.tensor([2 * 2 * 4]))
+        .reshape(10, 4, 2, 2)
+        .permute(1, 0, 2, 3)
+    )
 
     data = xr.Dataset(
         {
@@ -428,9 +433,9 @@ def dataset_input(normalize_pre_fill: bool, nan_fill_value: float):
     )
 
     test = DataSource("test", data, data_mean, data_std)
-    wet_surface = torch.ones(2, 2).bool()
-    wet_surface[0, 0] = False
-    wet_surface[1, 1] = False
+    wet_surface = torch.ones(2, 2)
+    wet_surface[0, 0] = 0.0
+    wet_surface[1, 1] = 0.0
     wet = wet_surface.expand(2, 2, 2)
 
     # Initialize and yield within the MultitonScope
@@ -447,7 +452,7 @@ def dataset_input(normalize_pre_fill: bool, nan_fill_value: float):
             boundary_var_names=boundary_var_names,
             wet=wet,
             wet_surface=wet_surface,
-            hist=0,
+            hist=1,
             steps=2,
             normalize_pre_fill=normalize_pre_fill,
             nan_fill_value=nan_fill_value,
@@ -459,7 +464,7 @@ def dataset_input(normalize_pre_fill: bool, nan_fill_value: float):
             boundary_var_names=boundary_var_names,
             wet=wet,
             wet_surface=wet_surface,
-            hist=0,
+            hist=1,
             normalize_pre_fill=normalize_pre_fill,
             nan_fill_value=nan_fill_value,
             long_rollout=True,
@@ -492,6 +497,16 @@ def test_train_dataset_normalize_pre_fill(
     td0 = traindataset[0]
 
     data = nan_fill_value
+
+    td0_step0_input = td0.get_input(0)
+    td0_step0_label = td0.get_label(0)
+    inf_step0_input, inf_step0_label = inference_dataset[0]
+
+    assert td0_step0_input.shape == (6, 2, 2)
+    assert td0_step0_label.shape == (4, 2, 2)
+    assert inf_step0_input.shape == (1, 6, 2, 2)
+    assert inf_step0_label.shape == (1, 4, 2, 2)
+
     if not normalize_pre_fill:
         mean = 0.5
         std = 1.0
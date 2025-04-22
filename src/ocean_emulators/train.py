60a670aa914558671c0bb3a88b92b0e427b3961c
Surya <surya.dheeshjith@gmail.com>
2025-04-22 16:01:01 -0700

    load checkpoint fixed

--- src/ocean_emulators/train.py
+++ src/ocean_emulators/train.py
@@ -8,6 +8,7 @@ import logging
 import os
 import time
 import warnings
+from collections import OrderedDict
 from collections.abc import Callable, Sequence
 from functools import partial
 from typing import Any, assert_never
@@ -277,6 +278,7 @@ class Trainer:
             cfg.resume_ckpt_path, cfg, finetune=cfg.finetune
         )
 
+        self.num_batches_seen = 0
         if cfg.resume_ckpt_path is not None:
             if cfg.finetune:
                 self.load_checkpoint(cfg.resume_ckpt_path, finetune=True)
@@ -324,7 +326,6 @@ class Trainer:
         self.inference_times = cfg.inference_times
         self.inference_epochs = cfg.inference_epochs
         self.time_delta: int = cfg.data.time_delta
-        self.num_batches_seen = 0
         self.ckpt_paths = CheckpointPaths(self.nets_dir)
         self.max_train_model_steps_forward = MAX_TRAIN_MODEL_STEPS_FORWARD // (
             self.hist + 1
@@ -806,6 +807,9 @@ class Trainer:
             "best_val_loss": self.best_val_loss,
             "best_inf_loss": self.best_inf_loss,
             "ema": self._ema.get_state(),
+            "num_batches_seen": self.num_batches_seen,
+            "wandb_id": self.wandb_id,
+            "wandb_name": self.wandb_name,
         }
         if self.scheduler:
             checkpoint["scheduler"] = self.scheduler.state_dict()
@@ -813,23 +817,41 @@ class Trainer:
         torch.save(checkpoint, checkpoint_path)
 
     def load_checkpoint(self, checkpoint_path, finetune=False):
-        logger.info(f"Loaded checkpoint from {checkpoint_path}")
-        checkpoint = torch.load(checkpoint_path)
-        if finetune:
-            self.model.load_state_dict(checkpoint["model"])
-        else:
+        logger.info(f"Loading checkpoint from {checkpoint_path}")
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device(self.device))
+
+        def remove_module_prefix(state_dict):
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k.removeprefix("module.")
+                new_state_dict[name] = v
+            return new_state_dict
+
+        # Load model state
+        model_state_dict = checkpoint["model"]
+        new_state_dict = remove_module_prefix(model_state_dict)
+        self.model.load_state_dict(new_state_dict)
+
+        # Load EMA state
+        model_ema_state_dict = checkpoint["ema"]
+        new_ema_state_dict = remove_module_prefix(model_ema_state_dict)
+        self._ema = EMATracker.from_state(new_ema_state_dict, self.model)
+
+        if not finetune:
             self.optimizer.load_state_dict(checkpoint["optimizer"])
-            if self.scheduler:
+            if self.scheduler and "scheduler" in checkpoint:
                 self.scheduler.load_state_dict(checkpoint["scheduler"])
+
             self.start_epoch = checkpoint["epoch"] + 1
-            self.wandb_id = checkpoint["wandb_id"]
-            self.wandb_name = checkpoint["wandb_name"]
+            self.wandb_id = checkpoint.get("wandb_id")
+            self.wandb_name = checkpoint.get("wandb_name")
+            self.num_batches_seen = checkpoint.get("num_batches_seen", 0)
 
             logger.info(f"Start Epoch: {self.start_epoch}")
             logger.info(f"Wandb id: {self.wandb_id}")
             logger.info(f"Wandb name: {self.wandb_name}")
-            logging.info(f"Optimizer LR: {self.optimizer.param_groups[-1]['lr']}")
-            self._ema = EMATracker.from_state(checkpoint["ema"], self.model)
+            logger.info(f"Optimizer LR: {self.optimizer.param_groups[-1]['lr']}")
+
             self.best_val_loss = checkpoint["best_val_loss"]
             self.best_inf_loss = checkpoint["best_inf_loss"]
 

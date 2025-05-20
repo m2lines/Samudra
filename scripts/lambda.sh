f8ae2e61e48415b077840cdbbc5ce04c61262b2c
Jesse Rusak <jesse@openathena.ai>
2025-05-20 16:01:42 -0400

    fix config

--- scripts/lambda.sh
+++ scripts/lambda.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 ROOT_DIR=$(pwd)
+BRANCH=u/jder/experiments/2025-05-20-halfdeg
+CONFIG=configs/train_om4_halfdeg.yaml
+SHARED_FS_NAME=jder-fs-1
 
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
-rclone config nyu-osn s3 --s3-provider Other --s3-endpoint https://nyu1.osn.mghpcc.org/ --all --s3-env-auth false # will prompt for auth
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+curl https://rclone.org/install.sh | sudo bash
+rclone config create nyu-osn s3 provider=Other endpoint=https://nyu1.osn.mghpcc.org/ --all # will prompt for auth
 
 mkdir data
-rclone copy nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr.zarr/ data/om4_sample_halfdeg_10yr/OM4.zarr
-rclone copy nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr_means.zarr/ data/om4_sample_halfdeg_10yr/OM4_means.zarr
-rclone copy nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr_std.zarr/ data/om4_sample_halfdeg_10yr/OM4_stds.zarr
+rclone copy --progress nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr.zarr/ data/om4_sample_halfdeg_10yr/OM4.zarr
+rclone copy --progress nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr_means.zarr/ data/om4_sample_halfdeg_10yr/OM4_means.zarr
+rclone copy --progress nyu-osn:emulators/sd5313/test/om4_sample_halfdeg_10yr_stds.zarr/ data/om4_sample_halfdeg_10yr/OM4_stds.zarr
 
 git clone git@github.com:suryadheeshjith/Ocean_Emulator.git
 cd Ocean_Emulator
+git switch $BRANCH
 
-echo "Run: uv run -m ocean_emulator.train configs/train_om4_halfdeg.yaml --cluster_data_dir=$ROOT_DIR/data/om4_sample_halfdeg_10yr --base_output_dir=$ROOT_DIR/jder-fs-1"
+echo "Run in a tmux: uv run torchrun --standalone --nnodes=1 --nproc-per-node=auto -m ocean_emulators.train $CONFIG --experiment.cluster_data_dir=$ROOT_DIR/data/om4_sample_halfdeg_10yr --experiment.base_output_dir=$ROOT_DIR/$SHARED_FS_NAME"

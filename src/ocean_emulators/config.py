0e31869aa37d0a6ce5187bfba7b11917c9e8fefc
Jesse Rusak <jesse@openathena.ai>
2025-06-12 19:47:31 -0400

    Revert "Switch to torch loader and reduce default number of workers per GPU (#255)"
    This reverts commit cd2e5d64d8d9b6ceb2a1ed6220c622dc00763ee9.

--- src/ocean_emulators/config.py
+++ src/ocean_emulators/config.py
@@ -93,9 +93,9 @@ class DataConfig(BaseConfig):
     data_stds_path: str = "CM4_5daily_v0.4.0_stds"
     scaling_residuals_file: str | None = None
     static_data_vars: list[str] | None = None
-    num_workers: int = 2
+    num_workers: int = 4
     hist: int = 1
-    loader_version: str = str(LoaderVersion.OM4_TORCH.value)
+    loader_version: str = str(LoaderVersion.OM4_EAGER.value)
     normalize_before_mask: bool = True
     masked_fill_value: float = 0.0
 

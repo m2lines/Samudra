ea4fb9343b5b9c2fb5da5fd9789fefc7c818c995
Jesse Rusak <jesse@openathena.ai>
2025-08-12 13:09:11 -0400

    Vizualization code (#339)
    This is an import and reorganization of the code from [this
    notebook](https://github.com/Open-Athena/Ocean_Emulator/blob/fixed-branch/Notebooks/notebooks/2025-04-08-Samudra_Eval_OM4.ipynb).
    The goal was to get it to a state we could check it in and iterate from
    there. This PR:
    
    * Moves that code from a notebook to python code in
    src/ocean_emulators/viz/core.py
    * Creates a new TopLevelConfig for visualization in
    src/ocean_emulators/viz/config.py (see CONTRIBUTING.md for an example
    invocation)
    * Does a minimal amount of deduplication, cleanup, and typing of the
    code there to get it passing CI and to allow the config to configure all
    the main knobs. Also added some TODOs about clear things we might want
    to do next.
    * Includes a tool to compare old and new viz output -- this is how I
    verified that the previous notebook and this code produce nearly
    identical results (modulo some FP error due to slight changes in
    ordering, I think).
    * Includes pointers to the basin data, now in the emulator pod, which
    was created by the new `notebooks/regrid_basins.py` code, which I
    included for posterity but don't think we really need to maintain per
    se.
    
    I would not try to review core.py too deeply but skimming the structure
    is good; same with notebooks/regrid_basins.py. Everything else is fair
    game :)

--- src/ocean_emulators/config_schema.py
+++ src/ocean_emulators/config_schema.py
@@ -8,6 +8,7 @@ import pydantic
 import yaml
 
 from ocean_emulators.config import EvalConfig, TrainConfig
+from ocean_emulators.viz import VizConfig
 
 
 def get_pydantic_models(
@@ -139,6 +140,7 @@ def main():
     # Get all available models
     models = get_pydantic_models(TrainConfig)
     models.update(get_pydantic_models(EvalConfig))
+    models.update(get_pydantic_models(VizConfig))
 
     generate_schemas(args.output_dir, models)
 
---
+++ src/ocean_emulators/viz/__init__.py
@@ -0,0 +1,4 @@
+from ocean_emulators.viz.config import VizConfig
+from ocean_emulators.viz.core import Viz, VizRun
+
+__all__ = ["Viz", "VizConfig", "VizRun"]

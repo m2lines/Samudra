41564666409ad63c3b60609a6ca4713390e9b0a8
Alex Merose <alex@openathena.ai>
2025-08-26 16:23:38 -0700

    Implementing Warmup + Cosine Decay LR schedule from ConvNeXt paper. (#343)
    Here is a Warmup implementation. This was added to match the LR schedule
    of the original ConvNeXt paper. Following the citations, warmup is
    theorhetical grounded and empirically verified to help the loss curves
    of networks with _large batch sizes_ -- see this paper:
    https://arxiv.org/pdf/1706.02677
    
    We have very small batch sizes, though each sample is fairly large (~80
    MB). This technique is not likely to lead to a modeling improvement;
    however, I've queued up a few jobs to test if this improves anything.
    While this might not make it into the final baseline Samudra config, I
    could see it being valuable for the next iteration of the model.
    
    ---------
    
    Co-authored-by: Jesse Rusak <jesse@openathena.ai>

--- src/ocean_emulators/utils/schedule.py
+++ src/ocean_emulators/utils/schedule.py
@@ -39,4 +39,40 @@ class CosineWithTailSchedulerConfig(BaseModel):
         )
 
 
-SchedulerConfig = CosineSchedulerConfig | CosineWithTailSchedulerConfig
+class CosineWithWarmupConfig(BaseModel):
+    """Cosine scheduler which goes from warmup_lr to the default lr for the first warmup_epochs."""
+
+    type: Literal["cosine_with_warmup"] = "cosine_with_warmup"
+
+    warmup_lr: float = 1e-6
+    warmup_epochs: int = 5
+
+    def build(
+        self, optimizer: torch.optim.Optimizer, epochs: int
+    ) -> torch.optim.lr_scheduler.LRScheduler:
+        assert len(optimizer.param_groups) == 1, (
+            "There can only be one parameter group for the optimizer."
+        )
+        warmup = torch.optim.lr_scheduler.LinearLR(
+            optimizer,
+            start_factor=self.warmup_lr / optimizer.param_groups[0]["lr"],
+            end_factor=1.0,  # Reaches full LR
+            total_iters=self.warmup_epochs,
+        )
+
+        assert self.warmup_epochs <= epochs, (
+            "'warmup_epochs' is too big; it must be smaller than 'epochs'."
+        )
+        cosine = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=epochs - self.warmup_epochs
+        )
+        return torch.optim.lr_scheduler.SequentialLR(
+            optimizer,
+            schedulers=[warmup, cosine],
+            milestones=[self.warmup_epochs],
+        )
+
+
+SchedulerConfig = (
+    CosineSchedulerConfig | CosineWithTailSchedulerConfig | CosineWithWarmupConfig
+)

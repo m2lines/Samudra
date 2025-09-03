bd86df19cedc25187237b5e9cccd295e17f48432
Alex Merose <alex@openathena.ai>
2025-09-03 13:19:45 -0700

    Added simple 2d patch embedding module for the FOMO encoder.
    Soon, we will add a Perceiver-based encoder module to the project. Before applying the Perceiver module, the first step we take on is to break up the input into ViT-style patches. This PR introduces a simple patch embedding mechanism in prep for the rest of the encoder.
    
    Fixes #362.

---
+++ src/ocean_emulators/models/modules/patchembed.py
@@ -0,0 +1,81 @@
+# Sources inspired by the following implementations:
+# - https://github.com/microsoft/aurora/blob/main/aurora/model/patchembed.py
+# - https://github.com/lucidrains/vit-pytorch
+
+from einops.layers.torch import Rearrange
+from jaxtyping import Array, Float
+from torch import nn
+
+from ocean_emulators.constants import Input
+
+
+class PatchEmbed2d(nn.Module):
+    """A patch embedding for Samudra's flattened data (the channel dim is a cross of variable, level, and time)."""
+
+    def __init__(
+        self,
+        input_vars: list[str],
+        patch_size: int | tuple[int, int] = 4,
+        embed_dim: int = 1024,
+        hist: int = 1,
+        norm: type[nn.Module] | None = nn.LayerNorm,
+    ) -> None:
+        """Embed TrainData arrays into patches.
+
+        Args:
+            input_vars (list[str]): list of input variable names. For input, this is typically the target prognostic
+              and boundary variable names.
+            patch_size (int): the size of the patches to embed. Patches must evenly divide the input grid. Further, the
+              grid dimension divided by the patch size must be greater than 16 pixels.
+            embed_dim (int): the dimension of the embedding to use.
+            hist (int): for the input channels, the number of additional time steps to include. With `hist=0`, it will
+              only include the present timestep. With `hist=1`, it will include the present and previous time step.
+            norm (type[nn.Module]): the normalization layer to use. This is applied both after creating patches and
+              after performing the linear projection.
+        """
+        super().__init__()
+        if isinstance(patch_size, int):
+            self.patch_size: tuple[int, int] = (patch_size, patch_size)
+        else:
+            self.patch_size = patch_size
+
+        self.embed_dim: int = embed_dim
+        self.n_channels = len(input_vars) * (1 + hist)
+
+        self.n_patches = patch_dim = (
+            self.n_channels * self.patch_size[0] * self.patch_size[1]
+        )
+
+        # While we could perform a patch embedding and linear projection in one step with a convolution, this
+        # implementation is much clearer. I don't expect that the additional computational cost to be that arduous.
+        self.patches = Rearrange(
+            "b v (h ph) (w pw) -> b (h w) (ph pw v)",
+            ph=self.patch_size[0],
+            pw=self.patch_size[1],
+        )
+        self.norm_patches = norm(patch_dim) if norm is not None else nn.Identity()
+        self.linear = nn.Linear(patch_dim, embed_dim)
+        self.norm_embedding = norm(embed_dim) if norm is not None else nn.Identity()
+
+    def forward(self, x: Input) -> Float[Array, "*batch patch_dim {self.embed_dim}"]:
+        B, V, H, W = x.shape
+
+        # V is a cross product of variable, level (encoded in vars), and time (has history).
+        assert V == self.n_channels
+
+        # Ensure patch_size is appropriate for the data.
+        assert H % self.patch_size[0] == 0, f"{H} %  {self.patch_size[0]} != 0."
+        assert (h := (H // self.patch_size[0]) ** 2) and h >= 16, (
+            f"{h=}: A picture is work 16x16 words!"
+        )
+        assert W % self.patch_size[1] == 0, f"{W} %  {self.patch_size[1]} != 0."
+        assert (w := (W // self.patch_size[1]) ** 2) and w >= 16, (
+            f"{w=}: A picture is work 16x16 words!"
+        )
+
+        x = self.patches(x)
+        x = self.norm_patches(x)
+        x = self.linear(x)
+        x = self.norm_embedding(x)
+
+        return x
---
+++ tests/test_patchembed.py
@@ -0,0 +1,121 @@
+import torch
+
+from ocean_emulators.config import TrainConfig
+from ocean_emulators.constants import BOUNDARY_VARS, PROGNOSTIC_VARS
+from ocean_emulators.datasets import TrainData
+from ocean_emulators.models.modules.patchembed import PatchEmbed2d
+
+from .test_datasets import make_loader
+
+
+def test_makes_patches():
+    x = torch.randn(1, 20, 180, 360)
+
+    patch_embed = PatchEmbed2d(
+        input_vars=[f"var_{i}" for i in range(x.shape[1])],
+        patch_size=4,
+        embed_dim=1024,
+        hist=0,
+        norm=None,
+    )
+
+    patches = patch_embed(x)
+
+    assert patches.shape == (1, 4050, 1024)
+
+
+def test_makes_patches__high_res():
+    x = torch.randn(1, 20, 360, 720)
+
+    patch_embed = PatchEmbed2d(
+        input_vars=[f"var_{i}" for i in range(x.shape[1])],
+        patch_size=4,
+        embed_dim=1024,
+        hist=0,
+        norm=None,
+    )
+
+    patches = patch_embed(x)
+
+    assert patches.shape == (1, 16200, 1024)
+
+
+def test_makes_patches__more_variables():
+    x = torch.randn(1, 71, 180, 360)
+
+    patch_embed = PatchEmbed2d(
+        input_vars=[f"var_{i}" for i in range(x.shape[1])],
+        patch_size=4,
+        embed_dim=1024,
+        hist=0,
+        norm=None,
+    )
+
+    patches = patch_embed(x)
+
+    assert patches.shape == (1, 4050, 1024)
+
+
+def test_makes_patches__with_layer_norm():
+    x = torch.randn(1, 20, 180, 360)
+
+    patch_embed = PatchEmbed2d(
+        input_vars=[f"var_{i}" for i in range(x.shape[1])],
+        patch_size=4,
+        embed_dim=1024,
+        hist=0,
+        norm=torch.nn.LayerNorm,
+    )
+
+    patches = patch_embed(x)
+
+    assert patches.shape == (1, 4050, 1024)
+
+
+def test_makes_patches__with_history():
+    x = torch.randn(1, 20, 180, 360)
+
+    patch_embed = PatchEmbed2d(
+        input_vars=[f"var_{i}" for i in range(x.shape[1] // 2)],
+        patch_size=4,
+        embed_dim=1024,
+        hist=1,
+        norm=None,
+    )
+
+    patches = patch_embed(x)
+
+    assert patches.shape == (1, 4050, 1024)
+
+
+def test_patch_embed__on_real_data(train_config: TrainConfig):
+    prognostic_vars = PROGNOSTIC_VARS[train_config.experiment.prognostic_vars_key]
+    boundary_vars = BOUNDARY_VARS[train_config.experiment.boundary_vars_key]
+    input_vars = prognostic_vars + boundary_vars
+
+    patch_embed = PatchEmbed2d(input_vars, hist=train_config.data.hist)
+
+    with make_loader(train_config) as loader:
+        for td in loader:
+            assert isinstance(td, TrainData), (
+                "The loader must return a TrainData! This is for the type checker."
+            )
+            # The forward pass of our base model manages a rollout across steps, where the model
+            # only ever needs to predict one step. There are two possible inputs in this step rollout
+            # system: the initial state, or a "merged" state, where we merge our latest prognostic
+            # prediction with the boundary forcings for that step.
+            # Thus, to ensure our patch encoder works as expected (for the one-step prediction part),
+            # we only need to test that the patch embedding works for either the initial input or the
+            # merged input.
+            initial_input = td.get_initial_input()
+            patches = patch_embed(initial_input)
+            assert patches.shape == (1, 4050, 1024)
+
+            prev_output = td.get_label(
+                max(0, len(td) - 2)
+            )  # Let's assume our predictor does a perfect job.
+            merged_input = td.merge_prognostic_and_boundary(
+                prognostic=prev_output, step=len(td) - 1
+            )
+            patches = patch_embed(merged_input)
+            assert patches.shape == (1, 4050, 1024)

51f2fbe353b3e7d3dc4579cea447e97a2787ab15
Alex Merose <alex@openathena.ai>
2025-09-19 10:09:10 -0700

    Decoder now using linear projection to unpatchify.
    This seems to be the standard ViT approach to reverse patches. I've also kept the simple Conv2d because I think it has some benefits (shared weights compared to a nn.Linear; better spatial reasoning; channel mixing is nice).

--- src/ocean_emulators/models/fomo.py
+++ src/ocean_emulators/models/fomo.py
@@ -1,5 +1,6 @@
 import torch
 import xarray as xr
+from einops import rearrange
 from torch import nn
 
 from ocean_emulators.constants import Grid
@@ -37,6 +38,8 @@ class FOMO(BaseModel):
             pad=pad,
             static_data=static_data,
         )
+        self.patch_size = encoder.patch_size
+
         # Placeholder decoder is a non-globe aware Conv2d.
         layers = [
             encoder,
@@ -44,8 +47,32 @@ class FOMO(BaseModel):
             nn.Conv2d(processor.out_channels, out_channels, last_kernel_size),
         ]
         self.layers = nn.ModuleList(layers)
+        self.unpatch = nn.Linear(
+            out_channels, out_channels * self.patch_size[0] * self.patch_size[1]
+        )
 
     def forward_once(self, fts: torch.Tensor) -> torch.Tensor:
+        _, _, H, W = fts.shape
+
         for layer in self.layers:
             fts = layer(fts)
+
+        # Get current patch-level dimensions
+        _, _out_channels, h_patches, w_patches = fts.shape
+
+        # project latent to output channels × patch area
+        fts = rearrange(fts, "b l h w -> b h w l")
+        fts = self.unpatch(fts)  # (b, h, w, out_channels * ph * pw)
+
+        # Unpatchify: reshape back to original spatial dimensions
+        fts = rearrange(
+            fts,
+            "b h w (c ph pw) -> b c (h ph) (w pw)",
+            c=self.out_channels,
+            ph=self.patch_size[0],
+            pw=self.patch_size[1],
+            h=h_patches,
+            w=w_patches,
+        )
+
         return torch.where(self.wet, fts, 0.0)

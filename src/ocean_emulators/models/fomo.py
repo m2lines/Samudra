5e169b79c5177f49dca689a9828a0d4594e1959b
Jesse Rusak <jesse@openathena.ai>
2025-09-30 12:10:28 -0400

    more

--- src/ocean_emulators/models/fomo.py
+++ src/ocean_emulators/models/fomo.py
@@ -2,14 +2,14 @@ import torch
 import xarray as xr
 from einops import rearrange
 from torch import nn
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    apply_activation_checkpointing,
+)
 
 from ocean_emulators.constants import Grid
 from ocean_emulators.models.base import BaseModel
 from ocean_emulators.models.modules import PerceiverEncoder
 from ocean_emulators.models.modules.unet_backbone import UNetBackbone
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    apply_activation_checkpointing,
-)
 
 
 class FOMO(BaseModel):
@@ -62,7 +62,7 @@ class FOMO(BaseModel):
         apply_activation_checkpointing(
             self,
             check_fn=lambda m: m.__class__.__name__
-            in ["LayerNorm", "FeedForward", "Linear"],
+            in ["LayerNorm", "FeedForward", "Linear", "Perceiver"],
         )
 
     def forward_once(self, fts: torch.Tensor) -> torch.Tensor:

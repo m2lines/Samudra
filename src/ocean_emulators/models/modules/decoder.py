2ea0c19dd3ddcdc51552302064a3c25535e340bb
Jesse Rusak <jesse@openathena.ai>
2025-09-30 11:24:04 -0400

    decoder sees only a given patch, not the whole ocean

--- src/ocean_emulators/models/modules/decoder.py
+++ src/ocean_emulators/models/modules/decoder.py
@@ -20,42 +20,41 @@ class PerceiverDecoder(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
-        grid_size: tuple[int, int],
+        patch_size: tuple[int, int],
         perceiver_depth: int,
         perceiver_latent_dim: int,
     ) -> None:
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.grid_size = grid_size
-
-        grid_area = grid_size[0] * grid_size[1]
+        self.patch_size = patch_size
 
         self.norm_patches = nn.LayerNorm(self.in_channels)
         self.perceiver = Perceiver(
             num_freq_bands=4,
-            max_freq=10.0,  # Depending on patch size and grid, consider values ranging from 3-10.
+            max_freq=self.in_channels,
             depth=perceiver_depth,
-            input_axis=2,  # Number of positional dims before token dim
+            input_axis=1,  # Number of positional dims before token dim
             input_channels=self.in_channels,
             latent_dim=perceiver_latent_dim,
-            num_classes=out_channels * grid_area,
+            num_classes=out_channels * patch_size[0] * patch_size[1],
             weight_tie_layers=True,  # share weights of cross-attn blocks
             self_per_cross_attn=2,  # ratio of self-attn (latent, small) and cross-attn (input, big) blocks
         )
-        self.norm_embedding = nn.LayerNorm(out_channels * grid_area)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = rearrange(x, "b l h w -> b h w l")
+        num_patches_h_w = x.shape[2:]
+        x = rearrange(x, "b l h w -> (b h w) l")  # h/w here are in units of patches
         x = self.norm_patches(x)
         x = self.perceiver(x)
-        x = self.norm_embedding(x)
         x = rearrange(
             x,
-            "b (h w c) -> b c h w",
+            "(b h w) (c p_h p_w) -> b c (h p_h) (w p_w)",
             c=self.out_channels,
-            h=self.grid_size[0],
-            w=self.grid_size[1],
+            p_h=self.patch_size[0],
+            p_w=self.patch_size[1],
+            h=num_patches_h_w[0],
+            w=num_patches_h_w[1],
         )
 
         return x

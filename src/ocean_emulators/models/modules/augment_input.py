580e80455bca97de27b73ada0c6b108568760ccd
Alexander Merose <alex@openathena.ai>
2025-11-13 10:20:15 -0800

    Compute grid once in init.

--- src/ocean_emulators/models/modules/augment_input.py
+++ src/ocean_emulators/models/modules/augment_input.py
@@ -9,30 +9,32 @@ from ocean_emulators.constants import Lat, Lon
 class Add3dCoordinates(nn.Module):
     """Add 3d Cartesian Coordinates on a unit sphere to the channel dimension.
 
+    3D coordinates are like so:
+    (0, 0, 0) is earth center.
+    (1, 0, 0) is at lat, lon = (0, 0)
+    (0, 1, 0) is at lat, lon = (0, 90)
+    (0, 0, 1) is at the North Pole
+
     This provides better pole handling than raw lat/lon coordinates.
     """
 
     def __init__(self, lat: Lat, lon: Lon):
         super().__init__()
-        self.lat, self.lon = lat, lon
-
-    def forward(
-        self, fts: Float[torch.Tensor, "batch channel height width"]
-    ) -> Float[torch.Tensor, "batch channel+3 height width"]:
-        lat_lon_grid = lat_lon_meshgrid(self.lat, self.lon)  # [2, H, W]
+        lat_lon_grid = lat_lon_meshgrid(lat, lon)  # [2, H, W]
         lat_rad = torch.deg2rad(lat_lon_grid[0])  # [H, W]
         lon_rad = torch.deg2rad(lat_lon_grid[1])  # [H, W]
 
-        # Compute 3D coordinates; earth center is (0, 0, 0)
-        # (1, 0, 0) is at lat, lon = (0, 0)
-        # (0, 1, 0) is at lat, lon = (0, 90)
-        # (0, 0, 1) is at the north pole
         x = torch.cos(lat_rad) * torch.cos(lon_rad)
         y = torch.cos(lat_rad) * torch.sin(lon_rad)
         z = torch.sin(lat_rad)
 
         grid = torch.stack([x, y, z], dim=0)  # [3, H, W]
-        grid = grid.float().to(fts.device).unsqueeze(0).expand(fts.shape[0], -1, -1, -1)
+        self.grid = grid.float().unsqueeze(0)
+
+    def forward(
+        self, fts: Float[torch.Tensor, "batch channel height width"]
+    ) -> Float[torch.Tensor, "batch channel+3 height width"]:
+        grid = self.grid.to(fts.device).expand(fts.shape[0], -1, -1, -1)
         fts = torch.cat((fts, grid), dim=1)
 
         return fts

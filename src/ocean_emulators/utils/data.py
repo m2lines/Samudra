61812b847ebd20862237ab902f80c5295c506788
Jesse Rusak <jesse@openathena.ai>
2025-11-26 15:28:48 -0500

    Remove unused method with a bad bug

--- src/ocean_emulators/utils/data.py
+++ src/ocean_emulators/utils/data.py
@@ -219,17 +219,6 @@ class DataSource:
         return norm
 
     @classmethod
-    def from_dataset(cls, dataset: xr.Dataset, *, name: str = "dataset") -> Self:
-        """Create a DataSource from the data, computing means and stds."""
-        # TODO(alxmrs): Support compact data
-        assert "lev" not in dataset.dims, "We currently only support non-compact data"
-
-        means = dataset.mean()
-        stds = dataset.std()
-
-        return cls(name=name, data=dataset, means=means, stds=stds)
-
-    @classmethod
     def from_locations(
         cls,
         data_location: ResolvedLocation,

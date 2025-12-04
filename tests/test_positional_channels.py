cb44ee79ba6272ce85e38780f83ae4eb30bc4578
Alexander Merose <alex@openathena.ai>
2025-12-04 14:18:39 -0800

    migrating more tests.

--- tests/test_positional_channels.py
+++ tests/test_positional_channels.py
@@ -4,7 +4,7 @@ import xarray as xr
 
 from ocean_emulators.config import SamudraConfig, UNetBackboneConfig
 from ocean_emulators.constants import TensorMap
-from ocean_emulators.utils.data import DataSource, Normalize
+from ocean_emulators.utils.data import MaskedDataSource, Masks, Normalize
 from ocean_emulators.utils.multiton import MultitonScope
 
 
@@ -29,7 +29,10 @@ def test_positional_parameters_update():
             },
             coords=coords,
         )
-        src = DataSource(name="dummy", data=data, means=data, stds=ones)
+        masks = Masks(torch.ones(h, w), torch.ones(h, w), torch.ones(h, w))
+        src = MaskedDataSource(
+            name="dummy", data=data, means=data, stds=ones, masks=masks
+        )
         Normalize.init_instance(
             src,
             TensorMap.get_instance().prognostic_var_names,

677b61fc07ea836aefb5eabd2404b2f548915b76
Alexander Merose <alex@openathena.ai>
2026-02-19 15:43:43 -0800

    no hardcoding in test code.

--- tests/test_validate_aggregator.py
+++ tests/test_validate_aggregator.py
@@ -1,31 +1,37 @@
-import pytest
 import torch
 
 from ocean_emulators.aggregator import ValidateAggregator
 from ocean_emulators.aggregator.train import TrainAggregator
 from ocean_emulators.aggregator.validate.sub_aggregator import ValidateSubAggregator
+from ocean_emulators.constants import TensorMap
 from ocean_emulators.utils.ctx import GridContext
 from ocean_emulators.utils.data import DataSource
 from ocean_emulators.utils.output import TrainBatchOutput, ValBatchOutput
 from ocean_emulators.utils.wandb import Metrics
 
 
-@pytest.fixture
-def val_batch() -> ValBatchOutput:
-    n_channels = 1
-    loss_per_channel = torch.ones(n_channels) * 1.5
+def src_to_val_batch(src: DataSource) -> ValBatchOutput:
+    """Create a dummy Validation Batch loss / data from a DataSource."""
+    # If we can consume a DataSource, then TensorMap has to be initialized.
+    tm = TensorMap.get_instance()
+    n_prog = len(tm.prognostic_var_names)
+    n_boundary = len(tm.boundary_var_names)
+
+    h, w = src.grid_size
+    loss_per_channel = torch.ones(n_prog) * 1.5
     loss = loss_per_channel.sum()
+
     batch = ValBatchOutput(
         loss=loss,
         loss_per_channel=loss_per_channel,
-        input_data=torch.randn(1, 2, 1, 4, 8),  # prog=1, boundary=1
-        target_data=torch.randn(1, 1, 1, 4, 8),
-        gen_data=torch.randn(1, 1, 1, 4, 8),
+        input_data=torch.randn(1, n_prog + n_boundary, 1, h, w),
+        target_data=torch.randn(1, n_prog, 1, h, w),
+        gen_data=torch.randn(1, n_prog, 1, h, w),
         ctx=GridContext(
-            label_mask=torch.ones(1, 1, 1, 4, 8),
+            label_mask=torch.ones(1, n_prog, 1, h, w),
             input_resolution_cpu=(
-                torch.linspace(-90, 90, steps=4),
-                torch.linspace(-180, 180, steps=8),
+                torch.linspace(-90, 90, steps=h),
+                torch.linspace(-180, 180, steps=w),
             ),
         ),
     )
@@ -33,6 +39,7 @@ def val_batch() -> ValBatchOutput:
 
 
 def val_to_train_batch(val_batch: ValBatchOutput) -> TrainBatchOutput:
+    """Ensures the TrainBatch loss is the same as the ValBatch loss."""
     return TrainBatchOutput(
         loss=val_batch.loss,
         loss_per_channel=val_batch.loss_per_channel,
@@ -60,8 +67,10 @@ class FakeSubAggregator(ValidateSubAggregator):
         self.num_recordings += 1
 
 
-def test_val_aggregator__no_op__is_same_as_train_aggregator(val_batch: ValBatchOutput):
-    val_agg = ValidateAggregator({}, hist=1, num_prognostic_channels=1)
+def test_val_aggregator__no_op__is_same_as_train_aggregator(dummy_src: DataSource):
+    val_batch = src_to_val_batch(dummy_src)
+    num_prog_channels = val_batch.loss_per_channel.shape[0]
+    val_agg = ValidateAggregator({}, hist=0, num_prognostic_channels=num_prog_channels)
     val_agg.record_validation_batch(val_batch)
     val_agg.record_validation_batch(val_batch)
 
@@ -76,10 +85,12 @@ def test_val_aggregator__no_op__is_same_as_train_aggregator(val_batch: ValBatchO
 
 
 def test_train_val_aggregator__with_fake_subagg__is_added_to_logs(
-    val_batch: ValBatchOutput, dummy_src: DataSource
+    dummy_src: DataSource,
 ):
+    val_batch = src_to_val_batch(dummy_src)
+    num_prog_channels = val_batch.loss_per_channel.shape[0]
     val_agg = ValidateAggregator(
-        {"fake": FakeSubAggregator()}, hist=1, num_prognostic_channels=1
+        {"fake": FakeSubAggregator()}, hist=0, num_prognostic_channels=num_prog_channels
     )
     val_agg.record_validation_batch(val_batch)
     val_agg.record_validation_batch(val_batch)

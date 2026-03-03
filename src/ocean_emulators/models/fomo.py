8dc7d81700ceeb6e0462986c98eaaf667ca817e8
Alex Merose <alex@openathena.ai>
2026-03-03 12:32:59 -0800

    Adding a perceiver-based decoder that complements the encoder. (#603)
    Here is a PerceiverIO-based decoder. It consumes latent information and
    produces the physical output grid with a single perceiver-transformer.
    It accomplishes this by arranging tensors to take in the latent input
    tokens and "query" channels, which are points on a 3D unit sphere that
    correspond to the area on Earth. Because the output dimension is large
    and involves expensive cross attentions, we provide a `window_size`
    argument to the decoder that will only process a chunk of output
    information at a time. This should bound peak memory during decoding.
    
    When the input patch extent is fine grained, attending over the whole
    input -- even when composed of latent information -- will likely explode
    memory during decoding. To address this, we provide the decoder with a
    `context_patches` that builds off the `window_size` argument: it adds a
    number of patches of `window_size` around the target patch as the only
    context (i.e. a local context) for the PerceiverIO call.
    
    Fixes #395.

--- src/ocean_emulators/models/fomo.py
+++ src/ocean_emulators/models/fomo.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
 import torch
-from einops import rearrange
 from perceiver_pytorch import Perceiver
 from perceiver_pytorch.perceiver_pytorch import Attention, FeedForward
 from torch import nn
@@ -9,10 +8,8 @@ from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     apply_activation_checkpointing,
 )
 
-from ocean_emulators.constants import GridSize
 from ocean_emulators.models.base import BaseModel
-from ocean_emulators.models.modules import PerceiverEncoder
-from ocean_emulators.models.modules.encoder import patch_from
+from ocean_emulators.models.modules import PerceiverDecoder, PerceiverEncoder
 from ocean_emulators.models.modules.unet_backbone import UNetBackbone
 from ocean_emulators.utils.ctx import GridContext
 from ocean_emulators.utils.device import autocast
@@ -37,10 +34,10 @@ class FOMO(BaseModel):
         add_3d_coordinates: nn.Module | None,
         encoder: PerceiverEncoder,
         processor: UNetBackbone,
+        decoder: PerceiverDecoder,
         hist: int,
         checkpointing: "Checkpointing | None",
         gradient_detach_interval: int,
-        grid_sizes: list[GridSize],
         use_bfloat16: bool,
     ):
         super().__init__(
@@ -56,27 +53,8 @@ class FOMO(BaseModel):
         self.maybe_add_3d_coordinates = add_3d_coordinates
         self.encoder = encoder
         self.processor = processor
+        self.decoder = decoder
         self.use_bfloat16 = use_bfloat16
-        # Placeholder decoder is a non-globe aware Conv2d.
-        self.decoder = nn.Conv2d(
-            processor.out_channels,
-            out_channels,
-            last_kernel_size,
-            padding=last_kernel_size // 2,
-        )
-        all_patches = [
-            patch_from(self.encoder.patch_extent, *grid_size)
-            for grid_size in grid_sizes
-        ]
-
-        self.unpatch = nn.ModuleDict(
-            {
-                str(patch_size): nn.Linear(
-                    out_channels, out_channels * patch_size[0] * patch_size[1]
-                )
-                for patch_size in all_patches
-            }
-        )
 
         if checkpointing == "all":
             apply_activation_checkpointing(
@@ -87,6 +65,7 @@ class FOMO(BaseModel):
                     | FeedForward
                     | nn.Linear
                     | Perceiver
+                    | PerceiverDecoder
                     | PerceiverEncoder
                     | UNetBackbone
                     | Attention,
@@ -94,31 +73,15 @@ class FOMO(BaseModel):
             )
 
     def forward_once(self, fts: torch.Tensor, ctx: GridContext) -> torch.Tensor:
-        _, _, H, W = fts.shape
-
         with autocast(enabled=self.use_bfloat16, dtype=torch.bfloat16):
             if self.maybe_add_3d_coordinates is not None:
                 fts = self.maybe_add_3d_coordinates(fts, ctx.input_resolution_cpu)
             fts = self.encoder(fts, ctx.input_resolution_cpu)
             fts = self.processor(fts)
+            fts = self.decoder(fts, ctx.input_resolution_cpu)
 
-        # Convert back to float32 for decoder and unpatchify operations
+        # Convert back to float32
+        # TODO(alxmrs): We actually only support float16 when turned on; this kind of tricks us.
         fts = fts.to(torch.float32)
-        fts = self.decoder(fts)
-
-        # Unpatchify: project to patch area, then reshape back to original spatial dimensions
-        patch_size = patch_from(self.encoder.patch_extent, H, W)
-        _, _, h, w = fts.shape
-        fts = rearrange(fts, "b l h w -> b h w l")
-        fts = self.unpatch[str(patch_size)](fts)  # (b, h, w, out_channels * ph * pw)
-        fts = rearrange(
-            fts,
-            "b h w (c ph pw) -> b c (h ph) (w pw)",
-            c=self.out_channels,
-            ph=patch_size[0],
-            pw=patch_size[1],
-            h=h,
-            w=w,
-        )
 
         return torch.where(ctx.label_mask, fts, 0.0)

33205707dec65b1282767d5d7babf8cc678bab24
github-action-benchmark <github@users.noreply.github.com>
2026-03-04 00:00:30 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for bd429fc66ebab925b8a1c08466d746a15ecb87f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772571566801,
+  "lastUpdate": 1772582430588,
   "repoUrl": "https://github.com/Open-Athena/Ocean_Emulator",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -8139,6 +8139,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.1072479256968355",
             "extra": "mean: 90.39039178519997 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alex@openathena.ai",
+            "name": "Alex Merose",
+            "username": "alxmrs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bd429fc66ebab925b8a1c08466d746a15ecb87f4",
+          "message": "Fixes to get flash perceiver working. (#619)\n\nClosing #617 in favor of this PR, which has a cleaner history. This was\ntested on a small Flash Perceiver model config on an A10 GPU on AWS.",
+          "timestamp": "2026-03-03T15:34:40-08:00",
+          "tree_id": "358b035a14df20a402f7e9433cbf874697a6ae06",
+          "url": "https://github.com/Open-Athena/Ocean_Emulator/commit/bd429fc66ebab925b8a1c08466d746a15ecb87f4"
+        },
+        "date": 1772582429753,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_datasets.py::test_profile__loader__1gb[LoaderVersion.OM4_TORCH-cpu-extra_config_args0-mock-test/train_default.yaml]",
+            "value": 1.1850708388762392,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0028136096865591753",
+            "extra": "mean: 843.8314125999966 msec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_datasets.py::test_profile__inference_loader__1gb[cpu-extra_config_args0-mock-test/train_default.yaml]",
+            "value": 0.1888526917142158,
+            "unit": "iter/sec",
+            "range": "stddev: 0.017500923935066033",
+            "extra": "mean: 5.2951323644000015 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_trainer.py::test_trainer__mini_benchmark[cpu-extra_config_args0-mock-test/train_default.yaml]",
+            "value": 0.011280598443626343,
+            "unit": "iter/sec",
+            "range": "stddev: 0.5184835495627772",
+            "extra": "mean: 88.64777919340001 sec\nrounds: 5"
           }
         ]
       }

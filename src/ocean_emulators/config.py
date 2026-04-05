459241f53cdfb3d07241841d8493b0e2a96ce700
GHOpenonic <codydcruz@outlook.com>
2026-04-05 11:29:08 -0400

    add group_norm handling for cpu vs gpu decompress parity

--- src/ocean_emulators/config.py
+++ src/ocean_emulators/config.py
@@ -279,7 +279,7 @@ class DataConfig(BaseConfig):
 
 BlockType = Literal["conv_next_block", "conv_block"]
 ActivationType = Literal["relu", "gelu", "capped_gelu"]
-NormType = Literal["batch", "instance", "layer"]
+NormType = Literal["batch", "instance", "group", "nonorm", "layer"]
 
 
 class BlockConfig(BaseConfig):
@@ -288,6 +288,7 @@ class BlockConfig(BaseConfig):
     activation: ActivationType = "capped_gelu"
     upscale_factor: int = 4
     norm: NormType = "batch"
+    group_norm_groups: int = 32
     pointwise_linear: bool = False
 
     def build(self) -> CoreBlockBuilder:
@@ -332,6 +333,7 @@ class BlockConfig(BaseConfig):
                         kernel_size=self.kernel_size,
                         upscale_factor=self.upscale_factor,
                         norm=self.norm,
+                        group_norm_groups=self.group_norm_groups,
                         activation=activation,
                         pointwise_linear=self.pointwise_linear,
                     )

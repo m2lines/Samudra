2f6ce5a51d3957bcb723d70335db303c2da5fb68
Alexander Merose <alex@openathena.ai>
2026-09-10 15:50:33 -0700

    Refresh September Xarray representations

--- tests/test_clone_data.py
+++ tests/test_clone_data.py
@@ -7,8 +7,7 @@ from scripts.clone_data import DEFAULT_DATA_ROOT, source_store
 
 def test_default_clone_source_uses_canonical_september_release():
     assert DEFAULT_DATA_ROOT == (
-        "https://nyu1.osn.mghpcc.org/m2lines-pubs/Samudra/"
-        "v2026-09/om4_onedeg/"
+        "https://nyu1.osn.mghpcc.org/m2lines-pubs/Samudra/v2026-09/om4_onedeg/"
     )
     assert source_store(DEFAULT_DATA_ROOT, "OM4") == f"{DEFAULT_DATA_ROOT}OM4.zarr"
 
